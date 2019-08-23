02e7ac2db04b17a73409e29eebbb170b599c46b4
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2019-08-22 17:56:25 -0700

    Migrate jest mocks to more logical placement, improve gatsby mock

--- config/jest/setup-test-env.js
+++ config/jest/setup-test-env.js
@@ -1,38 +1,5 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
-import {onLinkClick} from 'gatsby';
+import '../../__mocks__/modules/gatsby';
+import '../../__mocks__/modules/gatsby-image';
 
-jest.mock('gatsby-image', () => {
-  return (props) => {
-    return <img
-      src={props.fixed}
-      alt={props.alt}
-      data-testid={props['data-testid']}
-      className={props.className}
-    />;
-  }
-});
-
-jest.mock('gatsby', () => {
-  const react = require('react');
-  const gatsbyOGl = jest.requireActual('gatsby');
-  const onLinkClick = jest.fn();
-
-  return {
-    ...gatsbyOGl,
-    Link: react.forwardRef((props, ref) => {
-      return <a onClick={onLinkClick}
-         style={props.style}
-         ref={ref}
-         className={props.className}
-      >
-        {props.children}
-      </a>
-    }),
-    onLinkClick
-  }
-})
-
-afterEach(() => {
-  onLinkClick.mockReset();
-})
