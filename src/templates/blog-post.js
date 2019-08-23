b34af2d923a1ea780d82a9b57f6aa92839039f5b
Alex Dueppen <alex@adueppen.xyz>
2019-08-22 21:53:42 -0500

    add missing useContext import

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import { graphql } from "gatsby"
 import GitHubIcon from "../assets/icons/github.svg"
 import CommentsIcon from "../assets/icons/message.svg"
