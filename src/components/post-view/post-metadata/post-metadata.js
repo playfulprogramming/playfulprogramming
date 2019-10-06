b7a50a4aefab22141ecfccbaebcd491ff508e923
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2019-10-06 09:33:00 -0700

    Fix the post metadata header

--- src/components/post-view/post-metadata/post-metadata.js
+++ src/components/post-view/post-metadata/post-metadata.js
@@ -1,29 +1,61 @@
-import React, { useRef } from "react"
+import React, { createRef, useMemo, useRef } from "react"
 import Image from "gatsby-image"
 import styles from "./post-metadata.module.scss"
 import { Link } from "gatsby"
 
 export const PostMetadata = ({ post }) => {
   const { authors } = post.frontmatter
-  const authorLinkRef = useRef()
+  const authorLinkRefs = useMemo(() => authors.map(() => createRef()), [authors])
+
+  const getAuthorLinkHandler = i => () => authorLinkRefs[i].current.click()
+
+  const authorImgs = authors.map((author, i, arr) => {
+      const hasTwoAuthors = arr.length !== 1
+      const xyPosition =
+        !hasTwoAuthors ? undefined :
+          i === 1 ? "14px" : "-7px"
+      const classesToApplyToImg = hasTwoAuthors ? styles.twoAuthorPicSize : ""
+      const classesToApplyToContainer = hasTwoAuthors ? styles.twoAuthor : ""
+
+      return (
+        <div
+          key={i}
+          onClick={getAuthorLinkHandler(i)}
+          className={`pointer ${styles.profilePicLink} ${classesToApplyToContainer}`}
+          style={{
+            borderColor: author.color,
+            left: xyPosition,
+            top: xyPosition,
+          }}
+        >
+          <Image
+            className={`circleImg ${styles.authorPic} ${classesToApplyToImg}`}
+            fixed={author.profileImg.childImageSharp.mediumPic}
+            data-testid="post-meta-author-pic"
+            alt={`Profile pic for ${author.name}`}
+          />
+        </div>)
+    });
 
   return (
     <div className={styles.container}>
-      <div onClick={() => authorLinkRef.current.click()} className='pointer'>
-        <Image
-          className={`circleImg ${styles.authorPic}`}
-          fixed={authors[0].profileImg.childImageSharp.mediumPic}
-          data-testid="post-meta-author-pic"
-          alt={`Profile pic for ${authors[0].name}`}
-        />
-      </div>
+      <div className={styles.authorPicContainer}>{authorImgs}</div>
       <div className={styles.textDiv}>
-        <Link to={`/unicorns/${authors[0].id}`} ref={authorLinkRef} className={styles.authorLink}>
-          <h2 className={styles.authorName} data-testid="post-meta-author-name">{authors[0].name}</h2>
-        </Link>
+        <h2 className={styles.authorName} data-testid="post-meta-author-name">
+          {authors.map((author, i) =>
+            <Link
+              to={`/unicorns/${author.id}`}
+              ref={authorLinkRefs[i]}
+              className={styles.authorLink}
+            >
+              {i !== 0 && ', '}
+              {author.name}
+            </Link>
+          )}
+        </h2>
         <div className={styles.belowName}>
-          <p className={styles.date}>{post.frontmatter.published}</p>
-          <p className={styles.wordCount}>{post.wordCount.words} words</p>
+          <p>{post.frontmatter.published}</p>
+          <p>{post.wordCount.words} words</p>
         </div>
       </div>
     </div>
