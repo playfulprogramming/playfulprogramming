83fee81b734d7e7f6f35ecbef14777b00f638668
Corbin Crutchley <crutchcorn@gmail.com>
2020-03-14 20:47:19 -0700

    Fix errors in query, fix line-ending problems

--- plugins/count-inline-code/gatsby-node.js
+++ plugins/count-inline-code/gatsby-node.js
@@ -1,8 +1,16 @@
+// I would rather have these in their respective plugins folders, but Gatsby
+// didn't like having MarkdownRemarkFields implemented twice
 exports.createSchemaCustomization = ({ actions }) => {
 	const { createTypes } = actions;
 	const typeDefs = `
     type MarkdownRemarkFields implements Node {
       inlineCount: Int
+			headingsWithId: [HeadingsWithId]
+    }
+    type HeadingsWithId {
+      value: String!
+      depth: Int!
+      slug: String!
     }
   `;
 	createTypes(typeDefs);
