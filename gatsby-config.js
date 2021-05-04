89926f1bb1e937dff3fe54858c2b9908e8455151
Corbin Crutchley <crutchcorn@gmail.com>
2021-05-04 14:45:41 -0700

    fix: images no longer have white background

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,8 @@ module.exports = {
 						resolve: `gatsby-remark-images`,
 						options: {
 							maxWidth: 590,
-							linkImagesToOriginal: false
+							linkImagesToOriginal: false,
+							backgroundColor: 'transparent'
 						}
 					},
 					{
