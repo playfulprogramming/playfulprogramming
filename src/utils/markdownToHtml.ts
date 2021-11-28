435bf8a722577e8a22825a5e43b68ebce0e592f9
Corbin Crutchley <crutchcorn@gmail.com>
2021-11-28 01:09:13 -0800

    fix: image loading in blog posts

--- src/utils/markdownToHtml.ts
+++ src/utils/markdownToHtml.ts
@@ -8,10 +8,11 @@ import {postsDirectory} from "../api";
 
 // Optional now. Probably should move to an array that's passed or something
 // TODO: Create types
+import rehypeImageSize from 'rehype-img-size';
 const behead = require('remark-behead')
-const rehypeImageSize = require('rehype-img-size');
 
 export default async function markdownToHtml(slug: string, markdown: string) {
+  const imageDir = path.resolve(postsDirectory, slug);
   const result = await unified()
       .use(remarkParse)
       /* start remark plugins here */
@@ -22,7 +23,7 @@ export default async function markdownToHtml(slug: string, markdown: string) {
       /* start rehype plugins here */
       // TODO: https://github.com/ksoichiro/rehype-img-size/issues/4
       .use(rehypeImageSize, {
-        dir: path.resolve(postsDirectory, slug),
+        dir: imageDir,
       })
       /* end rehype plugins here */
       .use(rehypeStringify)
