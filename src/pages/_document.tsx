d790b36bd2ad6f6c77323ee18813b7d08252187e
Corbin Crutchley <crutchcorn@gmail.com>
2021-11-29 22:30:47 -0800

    Add back UU fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,20 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head >
+            <link
+                href="https://fonts.googleapis.com/css2?family=Work+Sans:400,500,600,700&display=swap"
+                rel="stylesheet"
+            />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Archivo:400,500,600&display=swap"
+                rel="stylesheet"
+            />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Roboto+Mono:400&display=swap"
+                rel="stylesheet"
+            />
+        </Head>
         <body>
           <Main />
           <NextScript />
