0c0035568fc7cb4e1d4db21d08375e911f53c505
Corbin Crutchley <crutchcorn@gmail.com>
2021-12-10 12:09:00 -0800

    Initial work on About page styling

---
+++ src/pages/about.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import style from "../page-components/about/about.module.scss";
+import { UnicornInfo } from "uu-types";
+import { unicorns } from "../api/get-datas";
+import unicornLogo from "../assets/unicorn_head_1024.png";
+import { useRouter } from "next/router";
+
+const getUnicornRoleListItems = (unicornInfo: UnicornInfo) => {
+  const unicornRoles = unicornInfo.roles.slice(0);
+
+  return unicornRoles.map((role, i, arr) => {
+    // If there is an item ahead
+    const shouldShowComma = arr[i + 1];
+    return (
+      <li key={role.id} role="listitem">
+        {role.prettyname}
+        {shouldShowComma && <span aria-hidden={true}>,&nbsp;</span>}
+      </li>
+    );
+  });
+};
+
+interface AboutUsProps {
+  allUnicorns: UnicornInfo[];
+}
+
+const AboutUs = ({ allUnicorns }: AboutUsProps) => {
+  const router = useRouter();
+
+  return (
+    <div>
+      {/*<SEO*/}
+      {/*  title={post.frontmatter.title}*/}
+      {/*  description={post.frontmatter.description || post.excerpt}*/}
+      {/*  pathName={props.location.pathname}*/}
+      {/*/>*/}
+      <div className={style.container}>
+        <div className={style.headerTitle}>
+          <div className={style.unicornLogo}>
+            <Image
+              src={unicornLogo}
+              loading={"eager"}
+              layout="responsive"
+              sizes={"192px"}
+              alt={"Unicorn Utterances logo"}
+            />
+          </div>
+          <h1>About Us</h1>
+        </div>
+        <main className={`${style.aboutBody} post-body`}>
+          {/*<div dangerouslySetInnerHTML={{ __html: post.html }} />*/}
+          {allUnicorns.map((unicornInfo) => {
+            const roleListItems = getUnicornRoleListItems(unicornInfo);
+
+            const navigateToUni = () =>
+              router.push(`/unicorns/${unicornInfo.id}`);
+
+            return (
+              <div key={unicornInfo.id} className={style.contributorContainer}>
+                <div
+                  className={`pointer ${style.userProfilePicture}`}
+                  onClick={navigateToUni}
+                >
+                  <Image
+                    alt={unicornInfo.name + " profile picture"}
+                    className="circleImg"
+                    layout="responsive"
+                    sizes="85px"
+                    height={unicornInfo.profileImg.height}
+                    width={unicornInfo.profileImg.width}
+                    src={unicornInfo.profileImg.relativeServerPath}
+                  />
+                </div>
+                <div className={style.nameRoleDiv}>
+                  <Link href={`/unicorns/${unicornInfo.id}`}>
+                    {unicornInfo.name}
+                  </Link>
+                  <ul
+                    aria-label="Roles assigned to this user"
+                    className={style.rolesList}
+                    role="list"
+                  >
+                    {roleListItems}
+                  </ul>
+                </div>
+              </div>
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  return {
+    props: {
+      allUnicorns: unicorns,
+    },
+  };
+}
+
+export default AboutUs;
