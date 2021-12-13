46eadaa97e640f55342f4d9a38eddf012f7db2ec
Corbin Crutchley <crutchcorn@gmail.com>
2021-12-12 22:56:42 -0800

    Add email confirm page

---
+++ src/pages/confirm.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import Image from "next/image";
+import { SEO } from "components/seo";
+import helloUnicorn from "assets/hello_2048.png";
+import { useRouter } from "next/router";
+
+const NotFoundPage = () => {
+  const router = useRouter();
+  return (
+    <>
+      <SEO title="Confirm Your Email" pathName={router.pathname} />
+      <div
+        style={{
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            width: "500px",
+            height: "500px",
+            background: "var(--primary)",
+            borderRadius: "100%",
+            overflow: "hidden",
+            margin: "0 auto",
+            display: "block",
+          }}
+        >
+          <Image
+            src={helloUnicorn}
+            loading={"eager"}
+            sizes="500px"
+            layout={"responsive"}
+            alt={`Unicorn Utterances 404 image`}
+          />
+        </div>
+      </div>
+      <h1 style={{ textAlign: "center" }}>Just one more thing...</h1>
+      <p style={{ textAlign: "center" }}>
+        Thank you for subscribing. You will need to check your inbox and confirm
+        your subscription.
+      </p>
+    </>
+  );
+};
+
+export default NotFoundPage;
