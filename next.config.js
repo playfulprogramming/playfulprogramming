c26c5ccc8678f557db6767673429f30cf7fb1060
Corbin Crutchley <crutchcorn@gmail.com>
2021-12-24 08:46:41 -0800

    chore: WIP adding in static image export

--- next.config.js
+++ next.config.js
@@ -1,47 +1,74 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([], {
-  webpack: (config) => {
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.resolve(__dirname, "content/blog"),
-            to: path.resolve(__dirname, "public/posts"),
-          },
-          {
-            from: path.resolve(__dirname, "content/data"),
-            to: path.resolve(__dirname, "public/unicorns"),
-          },
-        ],
-      })
-    );
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        inlineImageLimit: 1,
+        imagesName: "[name]-[hash].[ext]",
+        handleImages: ["png", "jpeg"],
+        responsive: {
+          adapter: require("responsive-loader/sharp"),
+          quality: 85,
+        },
+      },
+    ],
+  ],
+  {
+    trailingSlash: true,
+    compress: false, // NOTE: enable this when doing SSR
+    webpack: (config) => {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: path.resolve(__dirname, "content/blog"),
+              to: path.resolve(__dirname, "public/posts"),
+            },
+            {
+              from: path.resolve(__dirname, "content/data"),
+              to: path.resolve(__dirname, "public/unicorns"),
+            },
+          ],
+        })
+      );
 
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  name: "preset-default",
-                  params: {
-                    overrides: {
-                      removeViewBox: false,
+      config.resolve.symlinks = true;
+
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: "preset-default",
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
                     },
                   },
-                },
-              ],
+                ],
+              },
             },
           },
-        },
-      ],
-    });
+        ],
+      });
 
-    return config;
-  },
-});
+      return config;
+    },
+    devIndicators: {
+      autoPrerender: false,
+    },
+    images: {
+      disableStaticImages: true,
+    },
+  }
+);
