140bbda9ad693f690fce732fa7846dc169a07e4d
Corbin Crutchley <crutchcorn@gmail.com>
2022-01-07 02:46:09 -0800

    chore: add another test

--- jest.config.js
+++ jest.config.js
@@ -43,7 +43,7 @@ module.exports = {
     // UU Files
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.svg$": `<rootDir>/__mocks__/svg-comp-mock.ts`,
-    ".+\\.(jpg|svg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.ts`,
+    ".+\\.(jpg|svg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)(?:\\?.+)?$": `<rootDir>/__mocks__/file-mock.ts`,
     // UU TS
     "^__mocks__/(.*)$": resolve(__dirname, "./__mocks__/$1"),
     "^constants/(.*)$": resolve(__dirname, "./src/constants/$1"),
