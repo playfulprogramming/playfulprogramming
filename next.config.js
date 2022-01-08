354a8d83686412c1051560cb6befe46c317a73fb
Corbin Crutchley <crutchcorn@gmail.com>
2022-01-08 14:21:15 -0800

    Revert "chore: WIP adding in static image export"
    This reverts commit c26c5ccc8678f557db6767673429f30cf7fb1060.
    
    # Conflicts:
    #	next.config.js
    #	package-lock.json
    #	package.json

--- next.config.js
+++ next.config.js
@@ -1,84 +1,47 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
 
-const isNonDevBuild = process.env.BUILD_ENV !== "development";
-
-console.log({ isNonDevBuild });
-
-module.exports = withPlugins(
-  [
-    [
-      optimizedImages,
-      {
-        optimizeImagesInDev: isNonDevBuild,
-        optimizeImages: isNonDevBuild,
-        inlineImageLimit: 1,
-        imagesFolder: "images",
-        imagesPublicPath: "/_next/static/images/",
-        imagesOutputPath: "static/images/",
-        imagesName: "[name]-[hash].[ext]",
-        handleImages: ["png", "jpeg"],
-        responsive: {
-          disable: !isNonDevBuild,
-          adapter: require("responsive-loader/sharp"),
-          quality: 85,
-        },
-      },
-    ],
-  ],
-  {
-    trailingSlash: true,
-    compress: false, // NOTE: enable this when doing SSR
-    webpack: (config) => {
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [
-            {
-              from: path.resolve(__dirname, "content/blog"),
-              to: path.resolve(__dirname, "public/posts"),
-            },
-            {
-              from: path.resolve(__dirname, "content/data"),
-              to: path.resolve(__dirname, "public/unicorns"),
-            },
-          ],
-        })
-      );
-
-      config.resolve.symlinks = true;
-
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: [
+module.exports = withPlugins([], {
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
           {
-            loader: "@svgr/webpack",
-            options: {
-              svgoConfig: {
-                plugins: [
-                  {
-                    name: "preset-default",
-                    params: {
-                      overrides: {
-                        removeViewBox: false,
-                      },
-                    },
-                  },
-                ],
-              },
-            },
+            from: path.resolve(__dirname, "content/blog"),
+            to: path.resolve(__dirname, "public/posts"),
+          },
+          {
+            from: path.resolve(__dirname, "content/data"),
+            to: path.resolve(__dirname, "public/unicorns"),
           },
         ],
-      });
+      })
+    );
 
-      return config;
-    },
-    devIndicators: {
-      autoPrerender: false,
-    },
-    images: {
-      disableStaticImages: true,
-    },
-  }
-);
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+});
--- src/utils/markdown/MarkdownRenderer/media.tsx
+++ src/utils/markdown/MarkdownRenderer/media.tsx
@@ -1,37 +1,15 @@
 import * as React from "react";
 import { useMarkdownRendererProps } from "./types";
+import Image, { ImageProps } from "next/image";
 import Zoom from "react-medium-image-zoom";
 import { getFullRelativePath } from "utils/url-paths";
-import path from "path";
-
-/**
- * If this is not here, what will happen is that it will try to import markdown
- * files in `content/blog
- *
- * @see https://github.com/cyrilwanner/next-optimized-images/issues/16#issuecomment-405556547
- * @see https://webpack.js.org/guides/dependency-management/#requirecontext
- * @see https://github.com/Jam3/nextjs-boilerplate
- */
-const requireWebpImage = require.context(
-  "../../../../content",
-  true,
-  /\.(?:png|jpg|jpeg)$/
-);
 
 export const getMedia = ({ serverPath }: useMarkdownRendererProps) => {
   return {
-    img: (imgProps: HTMLImageElement) => {
-      const { src, height, width, ...props2 } = imgProps;
-      console.log({
-        serverPath: serverPath[0],
-        src,
-        val: getFullRelativePath(src),
-      });
+    img: (imgProps: unknown) => {
+      const { src, ...props2 } = imgProps as ImageProps;
+      let srcStr = getFullRelativePath(...serverPath, src as string); // ImageProps isn't _quite_ right for our usg here
 
-      const realSrc = requireWebpImage(
-        `./${getFullRelativePath(...serverPath, src)}`
-      );
-      
       const htmlProps = imgProps as Record<string, any>;
       const noZoomProp = htmlProps["data-nozoom"] ?? htmlProps?.dataset?.nozoom;
       const shouldZoom = noZoomProp ? noZoomProp === "false" : true;
@@ -40,9 +18,17 @@ export const getMedia = ({ serverPath }: useMarkdownRendererProps) => {
         ? Zoom
         : ((({ children }) => <>{children}</>) as React.FC);
 
+      // only "fill" is supported when height and width are not specified
+      const beResponsive = !!(props2.height && props2.width);
+
       return (
         <ZoomComp>
-          <img {...(props2 as any)} src={realSrc} />
+          <Image
+            src={srcStr}
+            {...props2}
+            layout={beResponsive ? "intrinsic" : "fill"}
+            loading="lazy"
+          />
         </ZoomComp>
       );
     },
