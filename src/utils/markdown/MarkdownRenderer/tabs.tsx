e22e66d235e0d358f93a5898b7ad0304906d057f
Corbin Crutchley <crutchcorn@gmail.com>
2022-01-10 17:42:26 -0800

    Get list of text of headers for tabs

--- src/utils/markdown/MarkdownRenderer/tabs.tsx
+++ src/utils/markdown/MarkdownRenderer/tabs.tsx
@@ -6,29 +6,33 @@ import {
   TabList as ReactTabList,
   TabPanel as ReactTabPanel,
 } from "react-tabs";
+import { ReactElement } from "react";
+import { onlyText } from "react-children-utilities";
 
 /**
  * @see https://github.com/reactjs/react-tabs for layout of "HTML" nodes
  */
 const Tabs: React.FC = ({ children }) => {
+  const tabsHeadingText = React.useMemo(() => {
+    const childrenArr = React.Children.toArray(children);
+    const tabList = childrenArr.filter(
+      (child) => (child as ReactElement).type === ReactTabList
+    )[0];
+    // A list of Tabs
+    const tabsCompList = (tabList as ReactElement).props.children;
+    const tabTextArr = tabsCompList.map((tabComp: ReactElement) => {
+      // Contents of tab header
+      return onlyText(tabComp.props.children);
+    });
+    return tabTextArr;
+  }, [children]);
   return <ReactTabs>{children}</ReactTabs>;
 };
 Tabs.displayName = "Tabs";
 
-const Tab: React.FC = ({ children }) => {
-  // TODO: use `Children.clone` to pass `index`
-  return <ReactTab>{children}</ReactTab>;
-};
-Tab.displayName = "Tab";
-
-const TabList: React.FC = ({ children }) => {
-  return <ReactTabList>{children}</ReactTabList>;
-};
-TabList.displayName = "TabList";
-
 export const getTabs = ({ serverPath }: useMarkdownRendererProps) => {
   return {
-    tabs: ReactTabs,
+    tabs: Tabs,
     tab: ReactTab,
     ["tab-list"]: ReactTabList,
     ["tab-panel"]: ReactTabPanel,
