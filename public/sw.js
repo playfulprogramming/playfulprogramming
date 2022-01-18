b136164f012e4de005fd6d085681f8ee6c322a03
Corbin Crutchley <crutchcorn@gmail.com>
2022-01-17 17:35:14 -0800

    Second attempt to remove old service worker

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,14 @@
-// Temporary to remove service workers from older versions of the Gatsby site
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .getRegistrations()
-    .then(function (registrations) {
-      for (let registration of registrations) {
-        registration.unregister();
-      }
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll();
     })
-    .catch(function (err) {
-      console.log("Service Worker registration failed: ", err);
+    .then(function (clients) {
+      clients.forEach((client) => client.navigate(client.url));
     });
-}
+});
