558efc292c95bc3c21b02d4afb655c41e93e113b
Corbin Crutchley <crutchcorn@gmail.com>
2022-03-10 21:10:01 -0800

    test: add tests for custom ID'd tabs

--- src/utils/markdown/plugins/tabs/tabs.spec.ts
+++ src/utils/markdown/plugins/tabs/tabs.spec.ts
@@ -92,3 +92,44 @@ Bonjour!
       }
     );
 });
+
+test("tabs can have custom IDs", (done) => {
+  unified()
+    .use(remarkParse)
+    .use(remarkStringify)
+    .use(remarkToRehype, { allowDangerousHtml: true })
+    .use(rehypeTabs, { tabSlugifyProps: { enableCustomId: true } })
+    .use(rehypeStringify, { allowDangerousHtml: true })
+    .process(
+      `
+<!-- tabs:start -->
+
+#### English {#test}
+
+Hello!
+
+#### French
+
+Bonjour!
+
+#### Italian
+
+Bonjour!
+
+<!-- tabs:end -->
+    `,
+      function (err, file) {
+        expect(err).toBeNull();
+        expect(file?.value).toMatchInlineSnapshot(`
+          "<tabs><tab-list><tab data-tabname=\\"test\\">English</tab><tab data-tabname=\\"french\\">French</tab><tab data-tabname=\\"italian\\">Italian</tab></tab-list><tab-panel data-tabname=\\"test\\">
+          <p>Hello!</p>
+          </tab-panel><tab-panel data-tabname=\\"french\\">
+          <p>Bonjour!</p>
+          </tab-panel><tab-panel data-tabname=\\"italian\\">
+          <p>Bonjour!</p>
+          <!-- tabs:end --></tab-panel></tabs>"
+        `);
+        done();
+      }
+    );
+});
