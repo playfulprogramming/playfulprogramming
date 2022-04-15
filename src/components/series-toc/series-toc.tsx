757e4c6e5901e004a9277c363cfaf87311594da5
Corbin Crutchley <git@crutchcorn.dev>
2022-04-14 18:00:52 -0700

    feat: initial work to get "see more" working in series TOC

--- src/components/series-toc/series-toc.tsx
+++ src/components/series-toc/series-toc.tsx
@@ -1,22 +1,28 @@
 import styles from "./series-toc.module.scss";
 import { RenderedPostInfo } from "types/PostInfo";
 import { SeriesPostInfo, SlugPostInfo } from "constants/queries";
-import { useRouter } from "next/router";
 import Link from "next/link";
+import { useMemo, useState } from "react";
 
 interface SeriesToCListItem {
   post: SeriesTocProps["postSeries"][0];
   isActive: boolean;
   partNum: number;
+  className?: string;
 }
 
-const SeriesToCListItem = ({ post, isActive, partNum }: SeriesToCListItem) => {
+const SeriesToCListItem = ({
+  post,
+  isActive,
+  partNum,
+  className,
+}: SeriesToCListItem) => {
   const liClass = isActive ? styles.isActive : "";
 
   const titleName = post.title.replace(new RegExp(`^${post.series}: `), "");
 
   return (
-    <li className={liClass || ""} role="listitem">
+    <li className={`${liClass || ""} ${className || ""}`} role="listitem">
       <Link href={`/posts/${post.slug}`} passHref>
         <a>
           Part {partNum}: {titleName}
@@ -25,6 +31,16 @@ const SeriesToCListItem = ({ post, isActive, partNum }: SeriesToCListItem) => {
     </li>
   );
 };
+
+function seperatePostsIntoThirds(seriesPosts: SeriesTocProps["postSeries"]) {
+  const firstPosts = seriesPosts.splice(0, 2);
+  const lastPosts = seriesPosts.splice(seriesPosts.length - 2, 2);
+  return {
+    firstPosts,
+    middlePosts: seriesPosts,
+    lastPosts,
+  };
+}
 
 interface SeriesTocProps {
   post: SlugPostInfo & RenderedPostInfo;
@@ -36,9 +52,18 @@ export const SeriesToC = ({
   postSeries,
   collectionSlug,
 }: SeriesTocProps) => {
-  const router = useRouter();
-
   const seriesText = `Part of our series: ${post.series}`;
+
+  const [areMiddlePostsActive, setMiddlePostsActive] = useState(false);
+
+  const { firstPosts, middlePosts, lastPosts } = useMemo(
+    () => seperatePostsIntoThirds(postSeries),
+    [postSeries]
+  );
+
+  const isActiveInMiddle = useMemo(() => {
+    return middlePosts.some((middlePost) => middlePost.order === post.order);
+  }, [middlePosts, post.order]);
 
   return (
     <div className={styles.seriesTableOfContent}>
@@ -52,7 +77,50 @@ export const SeriesToC = ({
         )}
       </div>
       <ol aria-labelledby="series-header" role="list">
-        {postSeries.map((seriesPost, i) => {
+        {firstPosts.map((seriesPost, i) => {
+          const isActive = post.order === seriesPost.order;
+
+          return (
+            <SeriesToCListItem
+              key={seriesPost.slug}
+              isActive={isActive}
+              partNum={i + 1}
+              post={seriesPost}
+            />
+          );
+        })}
+        {middlePosts.length !== 0 ? (
+          <>
+            <li
+              className={
+                areMiddlePostsActive
+                  ? styles.displayNone
+                  : isActiveInMiddle
+                  ? styles.isActive
+                  : ""
+              }
+              role="listitem"
+            >
+              <button aria-expanded={false}>
+                {middlePosts.length} more posts
+              </button>
+            </li>
+            {middlePosts.map((seriesPost, i) => {
+              const isActive = post.order === seriesPost.order;
+
+              return (
+                <SeriesToCListItem
+                  className={areMiddlePostsActive ? "" : styles.displayNone}
+                  key={seriesPost.slug}
+                  isActive={isActive}
+                  partNum={i + 1}
+                  post={seriesPost}
+                />
+              );
+            })}
+          </>
+        ) : null}
+        {lastPosts.map((seriesPost, i) => {
           const isActive = post.order === seriesPost.order;
 
           return (
