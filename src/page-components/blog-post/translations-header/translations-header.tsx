92cc4ded7bf63b99045825e2066d3eb956320039
Corbin Crutchley <git@crutchcorn.dev>
2022-05-13 11:09:01 -0700

    chore: improve styling of "this transated to" styling

--- src/page-components/blog-post/translations-header/translations-header.tsx
+++ src/page-components/blog-post/translations-header/translations-header.tsx
@@ -9,14 +9,17 @@ interface TranslationsHeaderProps {
 }
 export const TranslationsHeader = ({ post }: TranslationsHeaderProps) => {
   return (
-    <div>
-      Translated by readers into:{" "}
-      {(Object.keys(post.translations) as Languages[]).map((lang) => {
+    <div className={styles.translationContainer}>
+      Translated by our community into:{" "}
+      {(Object.keys(post.translations) as Languages[]).map((lang, i, arr) => {
         const langHref = lang === "en" ? "" : `${lang}/`;
         return (
-          <Link key={lang} passHref href={`/${langHref}posts/${post.slug}`}>
-            <a>{post.translations[lang]}</a>
-          </Link>
+          <>
+            <Link key={lang} passHref href={`/${langHref}posts/${post.slug}`}>
+              <a>{post.translations[lang]}</a>
+            </Link>
+            {i !== arr.length - 1 && <span>, </span>}
+          </>
         );
       })}
     </div>
