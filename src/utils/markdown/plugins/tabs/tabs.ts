2762bfc4436d82179ae6a6c822600fea73d17c47
Corbin Crutchley <git@crutchcorn.dev>
2022-06-11 02:31:20 -0700

    feat: fix tabs with non-raw HTML tags

--- src/utils/markdown/plugins/tabs/tabs.ts
+++ src/utils/markdown/plugins/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import { Root } from "hast";
 import replaceAllBetween from "unist-util-replace-all-between";
-import { Parent } from "unist";
+import { Parent, Node } from "unist";
 import { Plugin } from "unified";
 import { getHeaderNodeId, slugs } from "rehype-slug-custom-id";
 
@@ -52,96 +52,101 @@ export const rehypeTabs: Plugin<[RehypeTabsProps | never], Root> = ({
   tabSlugifyProps = {},
 }) => {
   return (tree) => {
+    const replaceTabNodes = (nodes: Node[]) => {
+      let sectionStarted = false;
+
+      const largestSize = findLargestHeading(nodes as ElementNode[]);
+
+      const tabsContainer = {
+        type: "element",
+        tagName: "tabs",
+        children: [
+          {
+            type: "element",
+            tagName: "tab-list",
+            children: [] as ElementNode[],
+          },
+        ],
+      };
+
+      for (const localNode of nodes as ElementNode[]) {
+        if (!sectionStarted && !isNodeLargestHeading(localNode, largestSize)) {
+          continue;
+        }
+        sectionStarted = true;
+
+        if (isNodeLargestHeading(localNode, largestSize)) {
+          // Make sure that all tabs labeled "thing" aren't also labeled "thing2"
+          slugs.reset();
+          const { id: headerSlug } = getHeaderNodeId(
+            localNode,
+            tabSlugifyProps
+          );
+
+          const header = {
+            type: "element",
+            tagName: "tab",
+            children: localNode.children,
+            properties: {
+              "data-tabname": headerSlug,
+            },
+          };
+
+          const contents = {
+            type: "element",
+            tagName: "tab-panel",
+            children: [],
+            properties: {
+              "data-tabname": headerSlug,
+            },
+          };
+
+          tabsContainer.children[0].children.push(header);
+
+          tabsContainer.children.push(contents);
+          continue;
+        }
+
+        if (isNodeHeading(localNode) && injectSubheaderProps) {
+          // This is `tagName: tab`
+          const lastTab =
+            tabsContainer.children[0].children[
+              tabsContainer.children[0].children.length - 1
+            ];
+
+          // Store the related tab ID in the attributes of the header
+          localNode.properties["data-tabname"] =
+            // Get the last tab's `data-tabname` property
+            lastTab.properties["data-tabname"];
+
+          // Add header ID to array
+          lastTab.properties["data-headers"] = JSON.stringify(
+            JSON.parse(lastTab.properties["data-headers"] ?? "[]").concat(
+              localNode.properties.id
+            )
+          );
+        }
+
+        // Push into last `tab-panel`
+        tabsContainer.children[tabsContainer.children.length - 1].children.push(
+          localNode
+        );
+      }
+
+      return [tabsContainer];
+    };
+
     replaceAllBetween(
       tree,
       { type: "raw", value: "<!-- tabs:start -->" } as never,
       { type: "raw", value: "<!-- tabs:end -->" } as never,
-      (nodes) => {
-        let sectionStarted = false;
-
-        const largestSize = findLargestHeading(nodes as ElementNode[]);
-
-        const tabsContainer = {
-          type: "element",
-          tagName: "tabs",
-          children: [
-            {
-              type: "element",
-              tagName: "tab-list",
-              children: [] as ElementNode[],
-            },
-          ],
-        };
-
-        for (const localNode of nodes as ElementNode[]) {
-          if (
-            !sectionStarted &&
-            !isNodeLargestHeading(localNode, largestSize)
-          ) {
-            continue;
-          }
-          sectionStarted = true;
-
-          if (isNodeLargestHeading(localNode, largestSize)) {
-            // Make sure that all tabs labeled "thing" aren't also labeled "thing2"
-            slugs.reset();
-            const { id: headerSlug } = getHeaderNodeId(
-              localNode,
-              tabSlugifyProps
-            );
-
-            const header = {
-              type: "element",
-              tagName: "tab",
-              children: localNode.children,
-              properties: {
-                "data-tabname": headerSlug,
-              },
-            };
-
-            const contents = {
-              type: "element",
-              tagName: "tab-panel",
-              children: [],
-              properties: {
-                "data-tabname": headerSlug,
-              },
-            };
-
-            tabsContainer.children[0].children.push(header);
-
-            tabsContainer.children.push(contents);
-            continue;
-          }
-
-          if (isNodeHeading(localNode) && injectSubheaderProps) {
-            // This is `tagName: tab`
-            const lastTab =
-              tabsContainer.children[0].children[
-                tabsContainer.children[0].children.length - 1
-              ];
-
-            // Store the related tab ID in the attributes of the header
-            localNode.properties["data-tabname"] =
-              // Get the last tab's `data-tabname` property
-              lastTab.properties["data-tabname"];
-
-            // Add header ID to array
-            lastTab.properties["data-headers"] = JSON.stringify(
-              JSON.parse(lastTab.properties["data-headers"] ?? "[]").concat(
-                localNode.properties.id
-              )
-            );
-          }
-
-          // Push into last `tab-panel`
-          tabsContainer.children[
-            tabsContainer.children.length - 1
-          ].children.push(localNode);
-        }
-
-        return [tabsContainer];
-      }
+      replaceTabNodes
+    );
+    replaceAllBetween(
+      tree,
+      { type: "comment", value: " tabs:start " } as never,
+      { type: "comment", value: " tabs:end " } as never,
+      replaceTabNodes
     );
     return tree;
   };
