e0b225ebe5bf522c642e161a049f93df33e289f1
Corbin Crutchley <git@crutchcorn.dev>
2022-07-06 06:09:29 -0700

    feat: remove images from markdown renderer

--- src/utils/markdown/MarkdownRenderer/media.tsx
+++ src/utils/markdown/MarkdownRenderer/media.tsx
@@ -7,33 +7,6 @@ import { EMBED_SIZE } from "../constants";
 
 export const getMedia = ({ serverPath }: useMarkdownRendererProps) => {
   return {
-    img: (imgProps: unknown) => {
-      const { src, style, ...props2 } = imgProps as any;
-      let srcStr = getFullRelativePath(...serverPath, src as string); // ImageProps isn't _quite_ right for our usg here
-
-      const htmlProps = imgProps as Record<string, any>;
-      const noZoomProp = htmlProps["data-nozoom"] ?? htmlProps?.dataset?.nozoom;
-      const shouldZoom = noZoomProp ? noZoomProp === "false" : true;
-
-      const ZoomComp = false
-        // ? Zoom
-        ? () => null 
-        : ((({ children }) => <>{children}</>) as any);
-
-      // only "fill" is supported when height and width are not specified
-      const beResponsive = !!(props2.height && props2.width);
-
-      return (
-        <ZoomComp>
-          {/* <Image
-            src={srcStr}
-            {...props2}
-            layout={beResponsive ? "intrinsic" : "fill"}
-            loading="lazy"
-          /> */}
-        </ZoomComp>
-      );
-    },
     video: (
       props: any
     ) => {
--- src/utils/markdown/markdownToHtml.ts
+++ src/utils/markdown/markdownToHtml.ts
@@ -10,16 +10,23 @@ import { rehypeHeaderText } from "./plugins/add-header-text";
 import remarkTwoslash from "remark-shiki-twoslash";
 import { UserConfigSettings } from "shiki-twoslash";
 import { rehypeTabs, RehypeTabsProps } from "utils/markdown/plugins/tabs";
+import rehypeSvimg from 'rehype-svimg';
 
 // TODO: Create types
 import behead from "remark-behead";
 import rehypeRaw from "rehype-raw";
 import { unifiedChain } from "utils/markdown/unified-chain";
 
+import * as path from 'path';
+import {postsDirectory } from "utils/fs/api";
+import { visit } from "unist-util-visit";
+
 export default async function markdownToHtml(
   content: string,
-  imgDirectory: string
+  slug: string
 ) {
+  const imgDirectory = path.resolve(postsDirectory, slug);
+
   // We mutate this object in plugins to add metadata from remark
   // rendering
   const renderData = {
@@ -75,6 +82,36 @@ export default async function markdownToHtml(
         } as RehypeTabsProps,
       ],
       [rehypeHeaderText(renderData)],
+      [rehypeSvimg, {
+        inputDir: imgDirectory,
+        outputDir: `public/posts/${slug}`,
+        webp: true,
+        avif: true,
+        generateImages: true,
+      }],
+      // TODO: Remove this when
+      // https://github.com/xiphux/rehype-svimg/issues/10
+      (() => tree => {
+        visit(tree, node => {
+          const prefix = 'public';
+          function removePrefix(path: string) {
+            if (path.startsWith(prefix)) {
+              if (path.startsWith(prefix + "/")) {
+                return path.slice(prefix.length + 1, path.length);
+              }
+              return path.slice(prefix.length, path.length);
+            }
+            return path;
+          }
+          if (node.tagName === 's-image') {
+            node.properties.src = "/" + removePrefix(node.properties.src );
+            node.properties.srcset = "/" + removePrefix(node.properties.srcset);
+            node.properties.srcsetwebp = "/" + removePrefix(node.properties.srcsetwebp);
+            node.properties.srcsetavif = "/" + removePrefix(node.properties.srcsetavif);
+          }
+        })
+        return tree;
+      })
     ],
   }).process(content);
 
