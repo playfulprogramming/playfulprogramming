886f209b7dd07df42375433dbd59dd18675e0272
Corbin Crutchley <git@crutchcorn.dev>
2022-07-09 04:36:58 -0700

    chore: initial work to migrate to Astro glob

--- src/types/modules/gatsby-remark-embedder.d.ts
+++ src/types/modules/gatsby-remark-embedder.d.ts
@@ -1,6 +1,6 @@
 declare module "gatsby-remark-embedder/dist/transformers/Twitch.js" {
   import { Transformer } from "@remark-embedder/core";
 
-  declare const transformer: Transformer<any>;
+  const transformer: Transformer<any>;
   export = transformer;
 }
