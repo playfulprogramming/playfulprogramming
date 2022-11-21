08dd3a303aa9c2f1c26592a093814397753404b8
Corbin Crutchley <git@crutchcorn.dev>
2022-11-21 08:28:48 -0800

    chore: initial work on the internals chapter cover

---
+++ src/page-components/collections/framework-field-guide/cover-layers/internals/clouds_first_layer.tsx
@@ -0,0 +1,14 @@
+export const CloudsFirstLayer = () => {
+	return (
+		<div
+			style={{
+                backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 1634 1680'%3E%3Cpath fill='%23A473BB' d='M0 962v-91a111 111 0 0 1 194-17 70 70 0 0 1 114 28 58 58 0 0 1 43 5 81 81 0 0 1 115-11 81 81 0 0 1 135 12 81 81 0 0 1 93-14 137 137 0 0 1 248-53 81 81 0 0 1 87 25h1c17 0 32 5 45 14a126 126 0 0 1 232 2 125 125 0 0 1 75-15 83 83 0 0 1 138 16 116 116 0 0 1 114 8v809H0V962Z'/%3E%3C/svg%3E")`,
+				width: '100%',
+				backgroundPosition: 'center center',
+				backgroundRepeat: "repeat no-repeat",
+				height: '100%',
+				backgroundSize: "auto 100%"
+			}}
+		/>
+	);
+};
