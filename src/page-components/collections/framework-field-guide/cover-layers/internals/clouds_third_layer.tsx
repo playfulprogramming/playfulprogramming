61b1e9d94671f53bed0c68f3410b2a9da1c5f2f4
Corbin Crutchley <git@crutchcorn.dev>
2022-11-22 03:32:12 -0800

    chore: update internals cover section

--- src/page-components/collections/framework-field-guide/cover-layers/internals/clouds_third_layer.tsx
+++ src/page-components/collections/framework-field-guide/cover-layers/internals/clouds_third_layer.tsx
@@ -2,12 +2,12 @@ export const CloudsThirdLayer = () => {
 	return (
 		<div
 			style={{
-				backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 1432 1680'%3E%3Cpath fill='%23422474' d='M0 512v1168h1432V512a82 82 0 0 0-114 1 65 65 0 0 0-51 7 103 103 0 0 0-196 43 95 95 0 0 0-114 50 60 60 0 0 0-46-6 75 75 0 0 0-96-8 82 82 0 0 0-132-20 59 59 0 0 0-109 12 75 75 0 0 0-18 2 76 76 0 0 0-132-34 89 89 0 0 0-97-7 127 127 0 0 0-251-16 50 50 0 0 0-76-24Z'/%3E%3C/svg%3E%0A")`,
+				backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 1432 1280'%3E%3Cpath fill='%23422474' d='M0 512v768h1432V512a82 82 0 0 0-114 1 65 65 0 0 0-51 7 103 103 0 0 0-196 43 95 95 0 0 0-114 50 60 60 0 0 0-46-6 75 75 0 0 0-96-8 82 82 0 0 0-132-20 59 59 0 0 0-109 12 75 75 0 0 0-18 2 76 76 0 0 0-132-34 89 89 0 0 0-97-7 127 127 0 0 0-251-16 50 50 0 0 0-76-24Z'/%3E%3C/svg%3E")`,
 				width: "100%",
 				backgroundPosition: "center center",
 				backgroundRepeat: "repeat no-repeat",
 				height: "100%",
-				backgroundSize: "auto 100%"
+				backgroundSize: "auto 100%",
 			}}
 		/>
 	);
