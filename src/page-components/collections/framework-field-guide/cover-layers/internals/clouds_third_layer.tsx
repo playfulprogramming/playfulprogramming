6999419621a56b4455e1b1b5254941acc5494c8b
Corbin Crutchley <git@crutchcorn.dev>
2022-11-28 21:32:34 -0800

    chore: get v1 of dynamic background color working

--- src/page-components/collections/framework-field-guide/cover-layers/internals/clouds_third_layer.tsx
+++ src/page-components/collections/framework-field-guide/cover-layers/internals/clouds_third_layer.tsx
@@ -1,8 +1,12 @@
+import { useCSSPropertyValue } from "../../hooks/use-css-property-value";
+
 export const CloudsThirdLayer = () => {
+	const cloudThirdColor = useCSSPropertyValue('--inter-700', "%23422474");
+
 	return (
 		<div
 			style={{
-				backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 1432 1280'%3E%3Cpath fill='%23422474' d='M0 512v768h1432V512a82 82 0 0 0-114 1 65 65 0 0 0-51 7 103 103 0 0 0-196 43 95 95 0 0 0-114 50 60 60 0 0 0-46-6 75 75 0 0 0-96-8 82 82 0 0 0-132-20 59 59 0 0 0-109 12 75 75 0 0 0-18 2 76 76 0 0 0-132-34 89 89 0 0 0-97-7 127 127 0 0 0-251-16 50 50 0 0 0-76-24Z'/%3E%3C/svg%3E")`,
+				backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 1432 1280'%3E%3Cpath fill='${cloudThirdColor}' d='M0 512v768h1432V512a82 82 0 0 0-114 1 65 65 0 0 0-51 7 103 103 0 0 0-196 43 95 95 0 0 0-114 50 60 60 0 0 0-46-6 75 75 0 0 0-96-8 82 82 0 0 0-132-20 59 59 0 0 0-109 12 75 75 0 0 0-18 2 76 76 0 0 0-132-34 89 89 0 0 0-97-7 127 127 0 0 0-251-16 50 50 0 0 0-76-24Z'/%3E%3C/svg%3E")`,
 				width: "100%",
 				backgroundPosition: "center center",
 				backgroundRepeat: "repeat no-repeat",
