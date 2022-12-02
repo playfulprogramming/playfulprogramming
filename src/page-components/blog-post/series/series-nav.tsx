6b2d7eb900aebb473c83f74926f7b405e421c370
James Fenn <me@jfenn.me>
2022-12-02 16:03:49 -0500

    fix next/before svg icons in series-nav

--- src/page-components/blog-post/series/series-nav.tsx
+++ src/page-components/blog-post/series/series-nav.tsx
@@ -1,6 +1,6 @@
 import styles from "./series-nav.module.scss";
-import {ReactComponent as NavigateNext} from "../../../icons/navigate_next.svg";
-import {ReactComponent as NavigateBefore} from "../../../icons/navigate_before.svg";
+import NavigateNext from "../../../icons/navigate_next.svg?raw";
+import NavigateBefore from "../../../icons/navigate_before.svg?raw";
 import { getShortTitle } from "./base";
 import { PostInfo } from "types/PostInfo";
 
@@ -8,6 +8,7 @@ interface SeriesNavProps {
   post: PostInfo;
   postSeries: PostInfo[];
 }
+
 export const SeriesNav = ({ post, postSeries }: SeriesNavProps) => {
   const postIndex = postSeries.findIndex((p) => p.order === post.order);
 
@@ -18,14 +19,14 @@ export const SeriesNav = ({ post, postSeries }: SeriesNavProps) => {
     <div className={styles.seriesNav}>
       {prevPost ? (
           <a href={`/posts/${prevPost.slug}`} className={`baseBtn prependIcon`}>
-            <NavigateBefore />
+            <span style={{ display: "inline-flex" }} dangerouslySetInnerHTML={{ __html: NavigateBefore }}></span>
             Previous Chapter: {getShortTitle(prevPost)}
           </a>
       ) : null}
       {nextPost ? (
           <a  href={`/posts/${nextPost.slug}`} className={`baseBtn appendIcon`}>
             Next Chapter: {getShortTitle(nextPost)}
-            <NavigateNext />
+            <span style={{ display: "inline-flex" }} dangerouslySetInnerHTML={{ __html: NavigateNext }}></span>
           </a>
       ) : (
         <div />
