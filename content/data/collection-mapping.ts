bc69d7d3c1cee22be148cd73ad9930d104d17c79
Corbin Crutchley <git@crutchcorn.dev>
2022-12-04 02:47:41 -0800

    chore: add custom mapping of collection items

--- content/data/collection-mapping.ts
+++ content/data/collection-mapping.ts
@@ -18,6 +18,21 @@ const collectionMapping: Array<RawCollectionInfo & { slug: string }> = [
 		],
 		customChaptersText: "3 books",
 	},
+	{
+		slug: "framework-field-guide-fundamentals",
+		title: "The Framework Field Guide - Fundamentals",
+		description:
+			"A practical and free way to teach Angular, React, and Vue all at once, so you can choose the right tool for the job and learn the underlying concepts in depth.",
+		authors: ["crutchcorn"],
+		coverImg: "/custom-content/collections/framework-field-guide/cover.png",
+		socialImg:
+			"/custom-content/collections/framework-field-guide/framework_field_guide_social.png",
+		type: "book",
+		tags: ["react", "angular", "vue"],
+		published: "2024-01-01T13:45:00.284Z",
+		buttons: [
+		],
+	},
 ];
 
 export default collectionMapping;
