5fd0b6297469c5d1f4cce48c119a97f14e7f9e8e
James Fenn <me@jfenn.me>
2023-03-01 01:14:17 -0500

    update button components to support more tags/args

--- src/components/base/button/button.tsx
+++ src/components/base/button/button.tsx
@@ -1,24 +1,26 @@
 import style from "./button.module.scss";
 
-interface ButtonProps {
-	href: string;
+type ButtonProps = {
+	tag?: "anchor" | "button";
 	class?: string;
 	state?: "selected" | "inactive";
 	variant?: "primary";
 	large?: boolean;
 	children: React.ReactNode,
-}
+} & React.ButtonHTMLAttributes<any> & React.AnchorHTMLAttributes<any>;
 
-export function Button({ href, class: className, children, ...props }: ButtonProps) {
+export function Button({ tag, class: className, children, state, variant, large, ...props }: ButtonProps) {
+	const Wrapper = (props: any) => tag === "button" ? <button {...props}/> : <a {...props}/>;
+
 	return (
-		<a href={href} class={[
-			!props.large && "text-style-button",
-			props.large && `text-style-button-large ${style.large}`,
-			style.button, className, style[props.variant], props.state,
+		<Wrapper {...props} aria-label={props["aria-label"]} class={[
+			!large && "text-style-button",
+			large && `text-style-button-large ${style.large}`,
+			style.button, className, style[variant], state,
 		].filter(c => !!c).join(" ")}>
-			<span class={style.buttonText}>
+			<span class={`d-flex ${style.buttonText}`}>
 				{children}
 			</span>
-		</a>
+		</Wrapper>
 	);
 }
