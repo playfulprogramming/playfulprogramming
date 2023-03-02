f3fa2d7970357f0ce7ab7dda2c4a7f9b202b14d3
James Fenn <me@jfenn.me>
2023-03-02 00:37:08 -0500

    add i18n utility for managing translation keys

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -1,5 +1,6 @@
 import { Languages } from "types/index";
 import { languages } from "constants/index";
+import { basename } from "path";
 
 /**
  * In our translations.json file, we choose to use a `eg-eg` format
@@ -18,6 +19,22 @@ export function fileToOpenGraphConverter<T extends Languages>(
 }
 
 /**
+ * Given a URL, find the prefix language.
+ *
+ * @example "/es/posts/test" -> "es"
+ * @example "/posts/test" -> "en"
+ * @example "/es-es/posts/test" -> "es-es"
+ */
+export function getPrefixLanguageFromPath(path: string): Languages {
+	// find the first non-empty path segment, e.g. ["", "en", "posts"] -> "en"
+	const pathSegment = path.split("/").find((s) => !!s);
+
+	if (Object.keys(languages).includes(pathSegment))
+		return pathSegment as Languages;
+	else return "en";
+}
+
+/**
  * Given a URL, remove the prefix language. Preserve whether or not `/` is present
  *
  * This is useful when trying to map out `hrefLang` and others
@@ -30,12 +47,62 @@ export function fileToOpenGraphConverter<T extends Languages>(
  * @example "es-es/posts/test" -> "posts/test"
  */
 export function removePrefixLanguageFromPath(path: string) {
-	const langs = Object.keys(languages) as Languages[];
-	const matchedLang = langs.find(
-		(lang) => path.startsWith(lang) || path.startsWith("/" + lang)
-	);
-	if (!matchedLang) return path;
-	if (path.startsWith("/")) return "/" + path.slice(matchedLang.length + 2);
-	// +1 since "es/path" needs to axe the trailing "/"
-	return path.slice(matchedLang.length + 1);
+	const matchedLang = getPrefixLanguageFromPath(path);
+
+	return path
+		.split("/")
+		.filter((s) => s !== matchedLang)
+		.join("/");
+}
+
+// fetch translation files from /data/i18n
+const i18n: Record<Languages, Map<string, string>> = Object.fromEntries(
+	await Promise.all(
+		Object.entries(import.meta.glob("../../content/data/i18n/*.json")).map(
+			async ([file, content]) =>
+				content().then((value: { default: Record<string, string> }) => {
+					return [
+						basename(file).split(".")[0],
+						new Map(Object.entries(value.default)),
+					];
+				})
+		)
+	)
+);
+
+// warn about any values that do not have full translations
+for (const key of i18n.en.keys()) {
+	const missing = Object.entries(i18n)
+		.filter(([, map]) => !map.has(key))
+		.map(([lang]) => lang);
+
+	if (missing.length) {
+		console.log(
+			`i18n: key "${key}" is missing from /content/data/i18n for languages: ${missing}`
+		);
+	}
+}
+
+/**
+ * Translate a key into the associated value, according to /data/i18n
+ *
+ * If the key is untranslated, returns the "en" value and logs a warning.
+ * If the key is entirely missing, throws an error.
+ */
+export function translate(astro: { url: URL }, key: string) {
+	const lang = getPrefixLanguageFromPath(astro.url.pathname);
+	let value = i18n[lang]?.get(key);
+
+	if (!value) {
+		console.warn(
+			`Translation key "${key}" is not specified in /content/data/i18n/${lang}.json`
+		);
+		value = i18n.en.get(key);
+	}
+
+	if (!value) {
+		throw `Translation key "${key}" does not exist.`;
+	}
+
+	return value;
 }
