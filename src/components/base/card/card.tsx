65f16d52cbc67cc376a2bfa80bb6c1333ab841ff
James Fenn <me@jfenn.me>
2023-03-02 23:04:32 -0500

    initial profile page view implementation

--- src/components/base/card/card.tsx
+++ src/components/base/card/card.tsx
@@ -4,17 +4,21 @@ import { createElement } from "preact";
 interface CardProps {
 	tag?: "li" | "div";
 	href?: string;
+	class?: string;
 	children: JSX.Element|JSX.Element[];
 }
 
-export function Card({ tag, children, ...props }: CardProps) {
+export function Card({ tag, children, class: className, ...props }: CardProps) {
 	const Wrapper = (props: any) => createElement(tag || "div", {
 		role: tag === "li" ? "listitem" : undefined,
 		...props,
 	}, props.children);
 
 	return (
-		<Wrapper class={style.card} onclick={props.href && `location.href='${props.href}'`}>
+		<Wrapper class={[
+			style.card, className,
+			props.href && style.interactive,
+		].filter(c => !!c).join(" ")} onclick={props.href && `location.href='${props.href}'`}>
 			{children}
 		</Wrapper>
 	);
