7d2b751543e0308b770205ed082f851c80dfb183
James Fenn <me@jfenn.me>
2023-04-01 19:16:17 -0400

    rm display styles, add unbordered tag component

--- src/components/base/tag/tag.tsx
+++ src/components/base/tag/tag.tsx
@@ -7,6 +7,14 @@ interface TagProps {
 
 export function Tag({ href, children, ...props }: TagProps) {
 	return (
+		<a class={`${style.tag} ${style.tagBordered} text-style-button`} href={href}>
+			{children}
+		</a>
+	);
+}
+
+export function TagLink({ href, children, ...props }: TagProps) {
+	return (
 		<a class={`${style.tag} text-style-button`} href={href}>
 			{children}
 		</a>
