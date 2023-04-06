3c6394f6c80c5f9589549b87de907af5616c5552
James Fenn <me@jfenn.me>
2023-04-06 19:43:03 -0400

    revert puppeteer package upgrade

--- build-scripts/social-previews/index.ts
+++ build-scripts/social-previews/index.ts
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 import { promises as fsPromises } from "fs";
 import { resolve } from "path";
 import { getAllExtendedPosts } from "utils/get-all-posts";
