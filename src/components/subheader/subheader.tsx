79157e3ecadf5f447bf772dbc09116ade626b18e
Corbin Crutchley <git@crutchcorn.dev>
2023-06-20 22:34:51 -0700

    chore: add subheader

---
+++ src/components/subheader/subheader.tsx
@@ -0,0 +1,26 @@
+import { PropsWithOptionalChildren } from "components/base/types";
+import styles from "./subheader.module.scss";
+import { createElement } from "preact";
+
+type SubHeaderProps = PropsWithOptionalChildren<{
+    tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+    class?: string;
+    style?: string;
+    text: string;
+}>;
+
+export function SubHeader({ tag, children, text, class: className, ...props }: SubHeaderProps) {
+    const Heading = (props: any) => createElement(tag, {
+        ...props,
+    }, props.children);
+
+    return (
+        <div className={styles.container}>
+            <Heading className={[
+                styles.heading,
+                className
+            ].filter(c => !!c).join(" ")}>{text}</Heading>
+            {children}
+        </div>
+    );
+}
