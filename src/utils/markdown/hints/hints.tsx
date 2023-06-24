7ad14469ccdf7c669d103276224efa9fab5f8216
James Fenn <me@jfenn.me>
2023-06-24 18:26:12 -0400

    embed svgs from build script to support dark mode

--- src/utils/markdown/hints/hints.tsx
+++ src/utils/markdown/hints/hints.tsx
@@ -1,5 +1,9 @@
 /** @jsxRuntime automatic */
 import { Node, Element } from "hast";
+import { fromHtml } from "hast-util-from-html";
+import { promises as fs } from "fs";
+
+const chevron_down = await fs.readFile("src/icons/chevron_down.svg", "utf8");
 
 interface HintProps {
 	title: string;
@@ -11,6 +15,7 @@ export function Hint({ title, children }: HintProps): Element {
 	return (
 		<details class="hint">
 			<summary class="hint__title text-style-body-medium-bold">
+				{fromHtml(chevron_down)}
 				{title}
 			</summary>
 
