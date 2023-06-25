58e9dd3a09b0cc694150866b7d3a1a8d6b2eb172
xenophorium <realqarnax@gmail.com>
2023-06-26 00:07:53 +0100

    fix: turn chip icon to separate prop to allow svg selection

--- src/components/chip/chip.tsx
+++ src/components/chip/chip.tsx
@@ -1,14 +1,16 @@
-import { PropsWithChildren } from "../types";
+import { JSXNode, PropsWithChildren } from "../types";
 import style from "./chip.module.scss";
 
 type ChipProps = PropsWithChildren<{
 	href?: string;
 	class?: string;
+	icon?: JSXNode;
 }>;
 
-export function Chip({ href, children }: ChipProps) {
+export function Chip({ href, icon, children }: ChipProps) {
 	return (
 		<a class={`${style.chip} text-style-button-regular`} href={href}>
+			{icon || ""}
 			<span class={`${style.chip_content}`}>{children}</span>
 		</a>
 	);
