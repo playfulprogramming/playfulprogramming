01d0733343af5c8f45c067a01ab1a8bc7bceea2d
xenophorium <realqarnax@gmail.com>
2023-06-26 18:51:23 +0100

    updated the chip style (again) to match the specs

--- src/components/chip/chip.tsx
+++ src/components/chip/chip.tsx
@@ -1,3 +1,4 @@
+import {Children} from 'react';
 import { PropsWithChildren } from "../types";
 import style from "./chip.module.scss";
 
@@ -9,7 +10,7 @@ type ChipProps = PropsWithChildren<{
 export function Chip({ href, children }: ChipProps) {
 	return (
 		<a class={`${style.chip} text-style-button-regular`} href={href}>
-			{children}
+			<span class={`${style.chip_content}`}>{children}</span>
 		</a>
 	);
 }
