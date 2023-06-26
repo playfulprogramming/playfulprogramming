d96d6acc368ef4ef487af31633b54726ed68fa5c
James Fenn <me@jfenn.me>
2023-06-25 23:04:49 -0400

    initial iframe placeholder styling

--- src/utils/markdown/iframes/iframe-placeholder.tsx
+++ src/utils/markdown/iframes/iframe-placeholder.tsx
@@ -13,29 +13,44 @@ export interface IFramePlaceholderProps {
 /** @jsxImportSource hastscript */
 export function IFramePlaceholder({ height, width, ...props }: IFramePlaceholderProps): Element {
 	return (
-		<div
-			class="iframe-replacement-container"
-			data-iframeurl={props.src}
-			style={`height: ${Number(height) ? `${height}px` : height}; width: ${Number(width) ? `${width}px` : width};`}
-		>
-			<picture>
-				{props.pageIcon.sources.map((source) => (
-					<source {...source} />
-				))}
-				<img
-					{...props.pageIcon.image as any}
-					class="iframe-replacement-icon"
-					alt=""
-					loading="lazy"
-					decoding="async"
-					data-nozoom="true"
-				/>
-			</picture>
-			<p class="iframe-replacement-title">
-				<span class="visually-hidden">An embedded webpage:</span>
-				{props.pageTitle}
-			</p>
-			<button class="iframe-replacement-button">Run embed</button>
+		<div class="embed">
+			<div class="embed__header">
+				<div class="embed__header__favicon">
+					<picture>
+						{props.pageIcon.sources.map((source) => (
+							<source {...source} />
+						))}
+						<img
+							{...props.pageIcon.image as any}
+							alt=""
+							loading="lazy"
+							decoding="async"
+							data-nozoom="true"
+						/>
+					</picture>
+				</div>
+				<div class="embed__header__info">
+					<p>
+						<span class="visually-hidden">An embedded webpage:</span>
+						{props.pageTitle}
+					</p>
+					<a
+						href={props.src}
+						rel="noreferrer"
+						target="_blank"
+					>
+						{props.src}
+					</a>
+				</div>
+				<button>New tab</button>
+			</div>
+			<div
+				class="embed__placeholder"
+				data-iframeurl={props.src}
+				style={`height: ${Number(height) ? `${height}px` : height};`}
+			>
+				<button>Run embed</button>
+			</div>
 		</div>
 	) as never;
 }
