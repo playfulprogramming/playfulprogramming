0dce5a9eea726d7129688020a076becbe50002d2
xenophorium <realqarnax@gmail.com>
2023-06-26 02:11:55 +0100

    fix icon and image display issues + label padding

--- src/components/chip/chip.tsx
+++ src/components/chip/chip.tsx
@@ -1,17 +1,15 @@
-import { JSXNode, PropsWithChildren } from "../types";
+import { PropsWithChildren } from "../types";
 import style from "./chip.module.scss";
 
 type ChipProps = PropsWithChildren<{
 	href?: string;
 	class?: string;
-	icon?: JSXNode;
 }>;
 
-export function Chip({ href, icon, children }: ChipProps) {
+export function Chip({ href, children }: ChipProps) {
 	return (
 		<a class={`${style.chip} text-style-button-regular`} href={href}>
-			{icon || ""}
-			<span class={`${style.chip_content}`}>{children}</span>
+			{children}
 		</a>
 	);
 }
