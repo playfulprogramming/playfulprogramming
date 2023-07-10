21a40e0f78d272c009430577ae7a73016bcdfeba
James Fenn <me@jfenn.me>
2023-07-09 20:25:51 -0400

    fix navigation-path-script selector

--- src/utils/navigation-path-script.ts
+++ src/utils/navigation-path-script.ts
@@ -1,23 +1,25 @@
 export const setupNavigationPaths = () => {
-	document.querySelectorAll(`[navigation-path]`).forEach((el) => {
-		const path = el.getAttribute("navigation-path");
+	document
+		.querySelectorAll(`[data-navigation-path]`)
+		.forEach((el: HTMLElement) => {
+			const path = el.dataset.navigationPath;
 
-		el.addEventListener("click", (e: MouseEvent) => {
-			// Ignore right-clicks
-			if (e.button !== 0) return;
-			// Download
-			if (e.altKey) return;
+			el.addEventListener("click", (e: MouseEvent) => {
+				// Ignore right-clicks
+				if (e.button !== 0) return;
+				// Download
+				if (e.altKey) return;
 
-			if (e.defaultPrevented) return;
+				if (e.defaultPrevented) return;
 
-			e.preventDefault();
+				e.preventDefault();
 
-			// Open in new tab
-			if (e.metaKey || e.ctrlKey || e.shiftKey) {
-				window.open(path, "_blank");
-				return;
-			}
-			location.href = path;
+				// Open in new tab
+				if (e.metaKey || e.ctrlKey || e.shiftKey) {
+					window.open(path, "_blank");
+					return;
+				}
+				location.href = path;
+			});
 		});
-	});
 };
