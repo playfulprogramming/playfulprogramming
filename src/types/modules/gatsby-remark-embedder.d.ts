09dfcac76e1b330599f78174b70f5cb13e02a6e9
Corbin Crutchley <git@crutchcorn.dev>
2023-07-24 04:00:44 -0700

    chore: delete unused types

--- src/types/modules/gatsby-remark-embedder.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "gatsby-remark-embedder/dist/transformers/Twitch.js" {
-	import { Transformer } from "@remark-embedder/core";
-
-	const transformer: Transformer<any>;
-	export = transformer;
-}
