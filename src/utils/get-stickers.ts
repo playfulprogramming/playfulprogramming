121318cd1fcec304dc84af702ce0f534e048bdb4
Corbin Crutchley <git@crutchcorn.dev>
2023-08-11 02:37:45 -0700

    chore: remove mobile sticker comp in favor of single sticker component with breakpoints

--- src/utils/get-stickers.ts
+++ src/utils/get-stickers.ts
@@ -1,41 +1,78 @@
-export const STICKER_SIZE = 144;
-
-export type StickerInfo = {
-	src: string;
-	width: number;
-	height: number;
-};
-
-export async function createSticker(
-	src: string,
-	width: number = STICKER_SIZE,
-	height: number = STICKER_SIZE,
-): Promise<StickerInfo> {
-	return {
-		src,
-		width,
-		height,
-	};
+/**
+ * These are the allowed stickers to be shown within our branding assets
+ *
+ * IE: Homepage, search page initial state, etc
+ *
+ * All of these stickers must be vetted manually for licencing rights and, if requested,
+ * must be removed.
+ *
+ * If you own the rights to these assets and would like yours removed, please email us at:
+ * licensing@unicorn-utterances.com
+ */
+export function getStickers() {
+	return [
+		{
+			src: "/stickers/android.svg",
+			explainer:
+				"https://developer.android.com/distribute/marketing-tools/brand-guidelines#android_robot",
+		},
+		{
+			src: "/stickers/angular.svg",
+			explainer: "https://angular.io/presskit#official-angular-logo",
+		},
+		{
+			src: "/stickers/css.svg",
+			explainer:
+				"https://en.wikipedia.org/wiki/CSS#/media/File:CSS3_logo_and_wordmark.svg",
+		},
+		{
+			src: "/stickers/html.svg",
+			explainer:
+				"https://en.wikipedia.org/wiki/HTML#/media/File:HTML5_logo_and_wordmark.svg",
+		},
+		{
+			src: "/stickers/python.svg",
+			explainer: "https://www.python.org/psf/trademarks/",
+		},
+		{
+			src: "/stickers/react.svg",
+			explainer:
+				"https://github.com/facebook/react/issues/12570#issuecomment-411130246",
+		},
+		{
+			src: "/stickers/typescript.svg",
+			explainer:
+				"https://github.com/microsoft/TypeScript-Website/blob/v2/LICENSE",
+		},
+		{
+			src: "/stickers/javascript.svg",
+			explainer: "https://github.com/voodootikigod/logo.js/blob/master/LICENSE",
+		},
+		{
+			src: "/stickers/linux.svg",
+			explainer:
+				"https://web.archive.org/web/20191001080512/https://isc.tamu.edu/~lewing/linux/",
+		},
+		{ src: "/stickers/vue.svg", explainer: "https://github.com/vuejs/art" },
+		{
+			src: "/stickers/ferris.svg",
+			explainer:
+				"https://foundation.rust-lang.org/policies/logo-policy-and-media-guide/",
+		},
+		{
+			src: "/stickers/git.svg",
+			explainer: "https://git-scm.com/downloads/logos",
+		},
+		{
+			src: "/stickers/cpp.svg",
+			explainer: "https://isocpp.org/home/terms-of-use",
+		},
+	] satisfies Sticker[];
 }
 
-export async function getStickers(): Promise<StickerInfo[]> {
-	return await Promise.all([
-		createSticker("/stickers/android.svg"),
-		createSticker("/stickers/angular.svg"),
-		createSticker("/stickers/css.svg"),
-		createSticker("/stickers/html.svg"),
-		createSticker("/stickers/python.svg"),
-		createSticker("/stickers/react.svg"),
-		createSticker("/stickers/typescript.svg"),
-		createSticker("/stickers/javascript.svg"),
-		createSticker("/stickers/linux.svg"),
-		createSticker("/stickers/vue.svg"),
-		createSticker(
-			"/stickers/ferris.svg",
-			STICKER_SIZE,
-			STICKER_SIZE * (399 / 512),
-		),
-		createSticker("/stickers/git.svg"),
-		createSticker("/stickers/cpp.svg"),
-	]);
+interface Sticker {
+	// The src on the Unicorn Utterances site
+	src: string;
+	// The explainer for why we should have the ability to display this logo
+	explainer: string;
 }
