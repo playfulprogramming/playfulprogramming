4a077149ac4c16b5dc92441ab5ad654861eaa40e
Corbin Crutchley <git@crutchcorn.dev>
2023-08-13 20:22:46 -0700

    chore: add initial stickers for desktop

--- src/views/search/components/search-hero.tsx
+++ src/views/search/components/search-hero.tsx
@@ -1,5 +1,25 @@
 import { JSXNode } from "components/types";
 import styles from "./search-hero.module.scss";
+import { getStickers } from "utils/get-stickers";
+
+const stickers = getStickers().sort(() => 0.5 - Math.random());
+
+const stickerTransforms = [
+	styles.sticker1,
+	styles.sticker2,
+	styles.sticker3,
+	styles.sticker4,
+	styles.sticker5,
+	styles.sticker6,
+	styles.sticker7,
+	styles.sticker8
+].map((className) => {
+	const sticker = stickers.pop();
+	return {
+		...sticker,
+		className,
+	};
+});
 
 interface SearchHeroProps {
 	title: string;
@@ -18,6 +38,12 @@ export const SearchHero = ({
 }: SearchHeroProps) => {
 	return (
 		<div class={styles.container}>
+			{
+				stickerTransforms.map((sticker) => (
+					<img aria-hidden="true" src={sticker.src} class={sticker.className} alt="" />
+				))
+			}
+
 			<img className={styles.image} src={imageSrc} alt={imageAlt} />
 			<h2 class={`text-style-headline-1 ${styles.title}`}>{title}</h2>
 			<p class={`text-style-body-medium ${styles.description}`}>
