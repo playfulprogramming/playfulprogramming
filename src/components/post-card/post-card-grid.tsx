435c111b83d53118ed5ba0b7f7420eb5a9fcd53f
Corbin Crutchley <git@crutchcorn.dev>
2023-08-18 01:44:32 -0700

    chore: improve accessibility of post/collection lists

--- src/components/post-card/post-card-grid.tsx
+++ src/components/post-card/post-card-grid.tsx
@@ -2,34 +2,31 @@ import style from "./post-card-grid.module.scss";
 import { PostCard, PostCardExpanded } from "./post-card";
 import { PostInfo } from "types/index";
 import { ProfilePictureMap } from "utils/get-unicorn-profile-pic-map";
+import { HTMLAttributes } from "preact/compat";
 
-export interface PostGridProps {
-	listAriaLabel: string;
+export interface PostGridProps extends HTMLAttributes<HTMLUListElement> {
 	postsToDisplay: PostInfo[];
 	unicornProfilePicMap: ProfilePictureMap;
 	expanded?: boolean;
 }
 
-export function PostCardGrid({ postsToDisplay, ...props }: PostGridProps) {
+export function PostCardGrid({
+	postsToDisplay,
+	unicornProfilePicMap,
+	expanded,
+	...props
+}: PostGridProps) {
 	return (
-		<ul
-			class={style.list}
-			aria-label={props.listAriaLabel}
-			role="list"
-			id="post-list-container"
-		>
+		<ul {...props} class={style.list} role="list" id="post-list-container">
 			{postsToDisplay.map((post) => {
-				return props.expanded && post.bannerImg ? (
+				return expanded && post.bannerImg ? (
 					<PostCardExpanded
 						class={style.expanded}
 						post={post}
-						unicornProfilePicMap={props.unicornProfilePicMap}
+						unicornProfilePicMap={unicornProfilePicMap}
 					/>
 				) : (
-					<PostCard
-						post={post}
-						unicornProfilePicMap={props.unicornProfilePicMap}
-					/>
+					<PostCard post={post} unicornProfilePicMap={unicornProfilePicMap} />
 				);
 			})}
 		</ul>
