6116decfb27b02932fd9a8ec70f46ea65b67a865
James Fenn <me@jfenn.me>
2023-09-27 23:28:08 -0400

    fix styling issues, allow dismissing by clicking outside the dialog

--- src/views/about/license.tsx
+++ src/views/about/license.tsx
@@ -15,7 +15,6 @@ export function License(props: LicenseProps) {
 
 	const handleOpen = (e: Event) => {
 		e.preventDefault();
-		console.log("open!");
 		setOpen(true);
 	};
 
@@ -25,8 +24,8 @@ export function License(props: LicenseProps) {
 
 	return (
 		<div class={style.license}>
-			<img width="24" height="24" src={props.image} loading="lazy" />
-			<div class="text-style-button-regular">
+			<img class={style.icon} width="24" height="24" src={props.image} loading="lazy" />
+			<div class={`text-style-button-regular ${style.info}`}>
 				<span>{props.name}</span>
 				<button class={style.viewButton} onClick={handleOpen}>{props.action}</button>
 			</div>
@@ -54,28 +53,32 @@ export function LicenseDialog({
 	const dialogRef = useRef<HTMLDialogElement>(null);
 
 	useEffect(() => {
-		console.log("showModal called", dialogRef.current);
 		dialogRef.current?.showModal();
 	}, [dialogRef.current]);
 
+	const handleClick = (e: Event) => {
+		if (e.target === dialogRef.current)
+			onClose();
+	};
+
 	return (
-		<dialog onClose={onClose} class={style.licenseDialog} ref={dialogRef}>
-			<div class={style.titleContainer}>
-				<LargeIconOnlyButton
-					tag="button"
-					type="button"
-					onClick={onClose}
-					class={style.closeButton}
-					aria-label="Close"
-				>
-					<span dangerouslySetInnerHTML={{ __html: close }} />
-				</LargeIconOnlyButton>
-				<h1 class={`text-style-headline-4 ${style.title}`}>{name}</h1>
-			</div>
-			<p>
-				The Android robot is reproduced or modified from work created and shared by Google and used according to terms described in
-				the <a href="https://example.com">Creative Commons</a> 3.0 Attribution License.
-			</p>
+		<dialog onClose={onClose} onClick={handleClick} class={style.licenseDialog} ref={dialogRef}>
+			<form method="dialog" class={style.form}>
+				<div class={style.titleContainer}>
+					<LargeIconOnlyButton
+						tag="button"
+						class={style.closeButton}
+						aria-label="Close"
+					>
+						<span style="display: flex;" dangerouslySetInnerHTML={{ __html: close }} />
+					</LargeIconOnlyButton>
+					<h1 class={`text-style-headline-4 ${style.title}`}>{name}</h1>
+				</div>
+				<div class="text-style-body-large">
+					The Android robot is reproduced or modified from work created and shared by Google and used according to terms described in
+					the <a href="https://example.com">Creative Commons</a> 3.0 Attribution License.
+				</div>
+			</form>
 		</dialog>
 	);
 }
