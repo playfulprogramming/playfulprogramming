92e7a1cda05609fa65d52914af50c79fe06cdd40
Corbin Crutchley <git@crutchcorn.dev>
2023-11-21 15:37:21 -0800

    docs: add 14 example

---
+++ content/blog/ffg-fundamentals-intro-to-components/ffg-fundamentals-vue-event-binding-14/src/FileList.vue
@@ -0,0 +1,11 @@
+<!-- FileList.vue -->
+<template>
+  <ul>
+    <li><File :fileName="'File one'" /></li>
+    <li><File :fileName="'File two'" /></li>
+    <li><File :fileName="'File three'" /></li>
+  </ul>
+</template>
+<script setup>
+import File from './File.vue';
+</script>
