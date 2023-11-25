0a1b99e52581640ed1efcce2f8372f96b2af2640
Corbin Crutchley <git@crutchcorn.dev>
2023-11-25 02:49:33 -0800

    docs: add fixed keyed demo example

---
+++ content/blog/ffg-fundamentals-dynamic-html/ffg-fundamentals-react-keyed-demo-22/src/main.jsx
@@ -0,0 +1,51 @@
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
+
+const WordList = () => {
+	const [words, setWords] = useState([]);
+
+	const addWord = () => {
+		const newWord = getRandomWord();
+		// Remove ability for duplicate words
+		if (words.includes(newWord)) return;
+		setWords([...words, newWord]);
+	};
+
+	const removeFirst = () => {
+		const newWords = [...words];
+		newWords.shift();
+		setWords(newWords);
+	};
+
+	return (
+		<div>
+			<button onClick={addWord}>Add word</button>
+			<button onClick={removeFirst}>Remove first word</button>
+			<ul>
+				{words.map((word) => {
+					return (
+						<li key={word.id}>
+							{word.word}
+							<input type="text" />
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
+
+const wordDatabase = [
+	{ word: "who", id: 1 },
+	{ word: "what", id: 2 },
+	{ word: "when", id: 3 },
+	{ word: "where", id: 4 },
+	{ word: "why", id: 5 },
+	{ word: "how", id: 6 },
+];
+
+function getRandomWord() {
+	return wordDatabase[Math.floor(Math.random() * wordDatabase.length)];
+}
+
+createRoot(document.getElementById("root")).render(<WordList />);
