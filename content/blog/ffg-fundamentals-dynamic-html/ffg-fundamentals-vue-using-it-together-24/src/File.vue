935a361d4cec7b1a1480e0cba1838a9f04155d52
Corbin Crutchley <git@crutchcorn.dev>
2023-11-26 05:26:41 -0800

    docs: add using it together code sample

---
+++ content/blog/ffg-fundamentals-dynamic-html/ffg-fundamentals-vue-using-it-together-24/src/File.vue
@@ -0,0 +1,25 @@
+<!-- File.vue -->
+<script setup>
+import FileDate from "./FileDate.vue";
+const inputDate = new Date();
+
+const props = defineProps(["isSelected", "isFolder", "fileName", "href"]);
+
+const emit = defineEmits(["selected"]);
+</script>
+
+<template>
+	<button
+		v-on:click="emit('selected')"
+		:style="
+			isSelected
+				? 'background-color: blue; color: white'
+				: 'background-color: white; color: blue'
+		"
+	>
+		{{ fileName }}
+		<span v-if="isFolder">Type: Folder</span>
+		<span v-else>Type: File</span>
+		<FileDate v-if="!isFolder" :inputDate="inputDate" />
+	</button>
+</template>
