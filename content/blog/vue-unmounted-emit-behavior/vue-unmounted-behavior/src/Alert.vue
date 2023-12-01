8cb3c97042c4b72b09977fde619d005b3afb6dce
Corbin Crutchley <git@crutchcorn.dev>
2023-12-01 08:25:13 -0800

    docs: add initial outline for vue unmounted blog post

---
+++ content/blog/vue-unmounted-emit-behavior/vue-unmounted-behavior/src/Alert.vue
@@ -0,0 +1,20 @@
+<!-- Alert.vue -->
+<script setup>
+import { onMounted } from "vue";
+
+const emit = defineEmits(["alert"]);
+const props = defineProps(["alert"]);
+
+// We are intentionally not cleaning up `onMounted`
+onMounted(() => {
+	setTimeout(() => {
+		emit("alert");
+		props.alert();
+		console.log("I am showing the alert");
+	}, 1000);
+});
+</script>
+
+<template>
+	<p>Showing alert...</p>
+</template>
