1bac30a2e3eb1a80f43ebe06cdfdae114f5f11c6
Corbin Crutchley <git@crutchcorn.dev>
2023-12-19 15:53:01 -0800

    docs: add server comps example

---
+++ content/blog/what-is-react-server-actions/nextjs-server-actions-server-comps/app/page.jsx
@@ -0,0 +1,25 @@
+import { addTodoToDatabase, getTodos } from "./todos";
+import { redirect } from "next/navigation";
+
+export default async function Todo() {
+	const todos = await getTodos();
+	async function addTodo(formData) {
+		"use server";
+		const todo = formData.get("todo");
+		await addTodoToDatabase(todo);
+		redirect("/");
+	}
+	return (
+		<>
+			<ul>
+				{todos.map((todo) => {
+					return <li key={todo.id}>{todo.value}</li>;
+				})}
+			</ul>
+			<form action={addTodo}>
+				<input name="todo" />
+				<button type="submit">Add Todo</button>
+			</form>
+		</>
+	);
+}
