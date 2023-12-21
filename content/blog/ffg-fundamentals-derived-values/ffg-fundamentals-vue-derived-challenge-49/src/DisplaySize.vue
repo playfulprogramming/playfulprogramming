2342060c39e24cf7699369cccc4ddf22c666ae0f
Corbin Crutchley <git@crutchcorn.dev>
2023-12-21 09:39:10 -0800

    docs: add code samples for derived embed

---
+++ content/blog/ffg-fundamentals-derived-values/ffg-fundamentals-vue-derived-challenge-49/src/DisplaySize.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["bytes"]);
+
+const kilobyte = 1024;
+const megabyte = kilobyte * 1024;
+const gigabyte = megabyte * 1024;
+
+function formatBytes(bytes) {
+	if (bytes < kilobyte) {
+		return `${bytes} B`;
+	} else if (bytes < megabyte) {
+		return `${Math.floor(bytes / kilobyte)} KB`;
+	} else if (bytes < gigabyte) {
+		return `${Math.floor(bytes / megabyte)} MB`;
+	} else {
+		return `${Math.floor(bytes / gigabyte)} GB`;
+	}
+}
+
+const humanReadableSize = computed(() => formatBytes(props.bytes));
+</script>
+
+<template>
+	<p>{{ humanReadableSize }}</p>
+</template>
