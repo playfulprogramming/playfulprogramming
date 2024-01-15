3865eae24b6d8d40de48135e73b340fc0c6e8bf0
Corbin Crutchley <git@crutchcorn.dev>
2024-01-15 01:01:16 -0800

    docs: add Vue challenge example for Direrctive chapter

---
+++ content/blog/ffg-fundamentals-directives/ffg-fundamentals-vue-directives-challenge/src/App.vue
@@ -0,0 +1,26 @@
+<!-- App.vue -->
+<script setup>
+import {vTooltip} from "./vTooltip.js";
+
+const obj = {current: null}
+</script>
+
+<template>
+	<div>
+    <!-- Ref doesn't appear to pass in time -->
+    <button :ref="el => obj.current = el">Hover me</button>
+    <!-- Anything passed to `v-tooltip` is not reactive, so this is the best we got -->
+    <div v-tooltip="obj">This is a tooltip</div>
+	</div>
+</template>
+
+<style>
+.tooltip {
+	position: absolute;
+	background-color: #333;
+	color: #fff;
+	padding: 8px;
+	border-radius: 4px;
+	z-index: 1000;
+}
+</style>
