86d9b4f4c8f4e4867e62ad09544d2179beb287d2
Corbin Crutchley <git@crutchcorn.dev>
2024-01-21 01:05:20 -0800

    docs: add error intro code samples

---
+++ content/blog/ffg-fundamentals-error-handling/ffg-fundamentals-vue-error-intro-70/src/App.vue
@@ -0,0 +1,17 @@
+<!-- App.vue -->
+<script setup>
+const items = [
+	{ id: 1, name: "Take out the trash", priority: 1 },
+	{ id: 2, name: "Cook dinner", priority: 1 },
+	{ id: 3, name: "Play video games", priority: 2 },
+];
+
+const priorityItems = items.filter((item) => item.item.priority === 1);
+</script>
+
+<template>
+	<h1>To-do items</h1>
+	<ul>
+		<li v-for="item of priorityItems" :key="item.id">{{ item.name }}</li>
+	</ul>
+</template>
