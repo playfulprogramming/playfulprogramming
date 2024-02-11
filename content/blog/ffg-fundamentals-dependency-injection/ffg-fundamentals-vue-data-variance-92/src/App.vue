6928e2021d0079781189381a3a77b7d982e0b4e8
Corbin Crutchley <git@crutchcorn.dev>
2024-02-11 02:10:27 -0800

    docs: data variance improvement samples

---
+++ content/blog/ffg-fundamentals-dependency-injection/ffg-fundamentals-vue-data-variance-92/src/App.vue
@@ -0,0 +1,17 @@
+<!-- App.vue -->
+<script setup>
+import { ref, provide } from "vue";
+import Child from "./Child.vue";
+
+const greeting = ref("");
+
+function changeGreeting(val) {
+	greeting.value = val;
+}
+
+provide("MESSAGE", { greeting, changeGreeting });
+</script>
+
+<template>
+	<Child />
+</template>
