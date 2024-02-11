7d1ddc563af1898c125c5ad9ad9cbebade5b3cb7
Corbin Crutchley <git@crutchcorn.dev>
2024-02-11 09:06:06 -0800

    docs: add buggy dialog code sample

---
+++ content/blog/ffg-fundamentals-portals/ffg-fundamentals-vue-buggy-dialog-94/src/Header.vue
@@ -0,0 +1,26 @@
+<!-- Header.vue -->
+<script setup>
+import FolderIcon from "./FolderIcon.vue";
+import DeleteIcon from "./DeleteIcon.vue";
+import Modal from "./Modal.vue";
+import { ref } from "vue";
+const shouldShowModal = ref(false);
+
+function showModal() {
+	shouldShowModal.value = true;
+}
+</script>
+
+<template>
+	<div class="header-container">
+		<Modal v-if="shouldShowModal" />
+		<span class="icon-container">
+			<FolderIcon />
+		</span>
+		<span class="header-title">Main folder</span>
+		<span class="auto"></span>
+		<button class="icon-btn" @click="showModal()">
+			<DeleteIcon />
+		</button>
+	</div>
+</template>
