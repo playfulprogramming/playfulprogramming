b9267828caa130409ce23f9f5b2623575fe8b43c
Corbin Crutchley <git@crutchcorn.dev>
2024-03-08 06:50:56 -0800

    chore: add ffg fundamentals social custom cover

--- content/data/collection-mapping.ts
+++ content/data/collection-mapping.ts
@@ -27,7 +27,7 @@ const collectionMapping: Array<RawCollectionInfo & { slug: string }> = [
 		coverImg:
 			"/custom-content/collections/framework-field-guide-fundamentals/cover.png",
 		socialImg:
-			"/custom-content/collections/framework-field-guide/framework_field_guide_social.png",
+			"/custom-content/collections/framework-field-guide-fundamentals/framework_field_guide_fundamentals_social.png",
 		type: "book",
 		tags: ["react", "angular", "vue"],
 		published: "2024-01-01T13:45:00.284Z",
