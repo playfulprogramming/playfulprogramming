79ad2750cdf4df0fdc7c11da4b98c4d2da8dece6
Mateusz Wąsik <wasik.matt@gmail.com>
2024-03-20 21:08:10 +0100

    feat: transform details and summary into divs, add unicode chevron arrow up for epub

--- src/utils/markdown/rehype-expand-details-summary.ts
+++ src/utils/markdown/rehype-expand-details-summary.ts
@@ -1,14 +1,43 @@
-import { BuildVisitor, visit } from "unist-util-visit";
+import { visit } from "unist-util-visit";
 import { Element, Root } from "hast";
+
+const unicodeElement: Element = {
+	type: "element",
+	tagName: "span",
+	properties: {
+		style: "margin-right: 3px;",
+	},
+	children: [
+		{
+			type: "text",
+			value: "\u25B2", // Unicode character for chevron up (▲)
+		},
+	],
+};
 
 export function rehypeExpandDetailsAndSummary() {
 	return (tree: Root) => {
-		function preVisitor(node: Element) {
+		function detailsAndSummaryVisitor(node: Element) {
 			if (node.tagName === "details") {
-				node.properties = { ...node.properties, open: true };
+				node.tagName = "div";
+				node.properties = {
+					...node.properties,
+					className: "hint__container",
+					open: true,
+				};
+			}
+			if (node.tagName === "summary") {
+				node.tagName = "div";
+				node.properties = {
+					...node.properties,
+					className: "hint__title text-style-body-medium-bold",
+				};
+				// for some reason chevron up icon is not showing up in the EPUB format
+				// we have to manually add this whole element as a first child
+				node.children.unshift(unicodeElement);
 			}
 		}
-		visit(tree, "element", preVisitor);
+		visit(tree, "element", detailsAndSummaryVisitor);
 		return tree;
 	};
 }
