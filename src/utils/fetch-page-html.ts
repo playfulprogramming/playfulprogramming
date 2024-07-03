f621deb807fe2e79fe070c44561abd2c44ea3f15
Corbin Crutchley <git@crutchcorn.dev>
2024-07-03 15:29:10 -0700

    chore: add Bot evasion and language acceptance to fetchPageHTML util

--- src/utils/fetch-page-html.ts
+++ src/utils/fetch-page-html.ts
@@ -8,7 +8,13 @@ export function fetchPageHtml(src: string): Promise<Root | null> {
 	if (pageHtmlMap.has(src)) return pageHtmlMap.get(src)!;
 
 	const promise = (async () => {
-		const srcHTML = await fetch(src)
+		const srcHTML = await fetch(src, {
+			headers: {
+				"User-Agent":
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+				"Accept-Language": "en",
+			},
+		})
 			.then((r) => (r.status === 200 ? r.text() : undefined))
 			.catch(() => null);
 
