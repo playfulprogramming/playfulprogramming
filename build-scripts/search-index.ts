454acc5b25d76da59e9c8fdf95a5d3ea568bd41b
Corbin Crutchley <git@crutchcorn.dev>
2024-08-21 17:25:33 -0700

    chore: update search index

--- build-scripts/search-index.ts
+++ build-scripts/search-index.ts
@@ -1,15 +1,43 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as api from "utils/api";
+import { PostInfo } from "types/PostInfo";
+import { PersonInfo } from "types/PersonInfo";
+import { CollectionInfo } from "types/CollectionInfo";
 
-const posts = api.getPostsByLang("en");
-const collections = api.getCollectionsByLang("en");
-const people = api.getPeopleByLang("en");
+interface ExtendedPostInfo extends PostInfo {
+	authorsMeta: PersonInfo[];
+}
+
+interface ExtendedCollectionInfo extends CollectionInfo {
+	authorsMeta: PersonInfo[];
+}
+
+const posts = api.getPostsByLang("en").map((post) => {
+	return {
+		...post,
+		authorsMeta: post.authors
+			.map((author) => {
+				return api.getPersonById(author, "en")!;
+			})
+			.filter(Boolean),
+	} as ExtendedPostInfo;
+});
+
+const collections = api.getCollectionsByLang("en").map((collection) => {
+	return {
+		...collection,
+		authorsMeta: collection.authors
+			.map((author) => {
+				return api.getPersonById(author, "en")!;
+			})
+			.filter(Boolean),
+	} as ExtendedCollectionInfo;
+});
 
 const json = JSON.stringify({
 	posts,
 	collections,
-	people,
 });
 
 fs.writeFileSync(
