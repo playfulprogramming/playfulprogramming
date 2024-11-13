60f72e2c95da5dacbc2309bd1aec5ba70b88ed9e
Corbin Crutchley <git@crutchcorn.dev>
2024-11-13 00:56:08 -0800

    docs: migrate sharted components 102 demo to Angular 19

--- content/crutchcorn/collections/framework-field-guide-fundamentals/posts/ffg-fundamentals-shared-component-logic/ffg-fundamentals-angular-composing-logic-102/src/main.ts
+++ content/crutchcorn/collections/framework-field-guide-fundamentals/posts/ffg-fundamentals-shared-component-logic/ffg-fundamentals-angular-composing-logic-102/src/main.ts
@@ -1,53 +1,45 @@
 import "zone.js";
 import { bootstrapApplication } from "@angular/platform-browser";
 
-import { Injectable, Component, inject, OnDestroy } from "@angular/core";
+import {
+	Injectable,
+	Component,
+	inject,
+	signal,
+	effect,
+	computed,
+} from "@angular/core";
 
 @Injectable()
-class WindowSize implements OnDestroy {
-	height = 0;
-	width = 0;
-
-	// We'll overwrite this behavior in another service
-	_listener!: () => void | undefined;
+class WindowSize {
+	height = signal(0);
+	width = signal(0);
 
 	constructor() {
-		this.onResize();
-		window.addEventListener("resize", this.onResize);
+		effect((onCleanup) => {
+			this.onResize();
+			window.addEventListener("resize", this.onResize);
+			onCleanup(() => {
+				window.removeEventListener("resize", this.onResize);
+			});
+		});
 	}
 
 	onResize = () => {
-		this.height = window.innerHeight;
-		this.width = window.innerWidth;
-		// We will call this "listener" function if it's present
-		if (this._listener) {
-			this._listener();
-		}
+		this.height.set(window.innerHeight);
+		this.width.set(window.innerWidth);
 	};
-
-	ngOnDestroy() {
-		window.removeEventListener("resize", this.onResize);
-	}
 }
 
 @Injectable()
 class IsMobile {
-	isMobile = false;
-
-	// We cannot use the `inject` function here, because we need to overwrite our `constructor` behavior
-	// and it's an either-or decision to use `constructor` or the `inject` function
-	constructor(private windowSize: WindowSize) {
-		windowSize._listener = () => {
-			if (windowSize.width <= 480) this.isMobile = true;
-			else this.isMobile = false;
-		};
-	}
+	windowSize = inject(WindowSize);
+	isMobile = computed(() => this.windowSize.width() <= 480);
 }
 
 @Component({
 	selector: "app-root",
-	standalone: true,
-	template: ` <p>Is mobile? {{ isMobile.isMobile }}</p> `,
+	template: ` <p>Is mobile? {{ isMobile.isMobile() }}</p> `,
 	providers: [WindowSize, IsMobile],
 })
 class AppComponent {
