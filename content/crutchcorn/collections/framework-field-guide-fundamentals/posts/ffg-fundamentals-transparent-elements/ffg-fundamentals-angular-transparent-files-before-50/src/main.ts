0fece08729a054c75057bd83eefec086fb768ca1
Corbin Crutchley <git@crutchcorn.dev>
2024-11-13 20:17:01 -0800

    docs: migrate all Angular code date samples back to OG string generation

--- content/crutchcorn/collections/framework-field-guide-fundamentals/posts/ffg-fundamentals-transparent-elements/ffg-fundamentals-angular-transparent-files-before-50/src/main.ts
+++ content/crutchcorn/collections/framework-field-guide-fundamentals/posts/ffg-fundamentals-transparent-elements/ffg-fundamentals-angular-transparent-files-before-50/src/main.ts
@@ -13,25 +13,16 @@ import {
 @Component({
 	selector: "file-date",
 	template: `
-		<span [attr.aria-label]="humanReadableDate()">
-			{{ displayDate() }}
+		<span [attr.aria-label]="labelText()">
+			{{ dateStr() }}
 		</span>
 	`,
 })
 class FileDateComponent {
 	inputDate = input.required<Date>();
 
-	displayDate = computed(() => {
-		return new Intl.DateTimeFormat("en-US").format(this.inputDate());
-	});
-
-	humanReadableDate = computed(() => {
-		return new Intl.DateTimeFormat("en-US", {
-			year: "numeric",
-			month: "long",
-			day: "numeric",
-		}).format(this.inputDate());
-	});
+	dateStr = computed(() => formatDate(this.inputDate()));
+	labelText = computed(() => formatReadableDate(this.inputDate()));
 }
 
 @Component({
@@ -165,6 +156,49 @@ interface File {
 	href: string;
 	isFolder: boolean;
 	id: number;
+}
+
+function formatDate(inputDate: Date) {
+	// Month starts at 0, annoyingly
+	const monthNum = inputDate.getMonth() + 1;
+	const dateNum = inputDate.getDate();
+	const yearNum = inputDate.getFullYear();
+	return monthNum + "/" + dateNum + "/" + yearNum;
+}
+
+function formatReadableDate(inputDate: Date) {
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	const monthStr = months[inputDate.getMonth()];
+	const dateSuffixStr = dateSuffix(inputDate.getDate());
+	const yearNum = inputDate.getFullYear();
+	return monthStr + " " + dateSuffixStr + "," + yearNum;
+}
+
+function dateSuffix(dayNumber: number) {
+	const lastDigit = dayNumber % 10;
+	if (lastDigit == 1 && dayNumber != 11) {
+		return dayNumber + "st";
+	}
+	if (lastDigit == 2 && dayNumber != 12) {
+		return dayNumber + "nd";
+	}
+	if (lastDigit == 3 && dayNumber != 13) {
+		return dayNumber + "rd";
+	}
+	return dayNumber + "th";
 }
 
 bootstrapApplication(FileListComponent);
