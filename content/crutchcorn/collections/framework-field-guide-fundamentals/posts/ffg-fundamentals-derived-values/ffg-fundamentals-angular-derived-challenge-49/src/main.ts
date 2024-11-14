a49f93f574865a07ea57022c0de6fa9aefb1cd0e
Corbin Crutchley <git@crutchcorn.dev>
2024-11-13 19:52:14 -0800

    docs: migrate derived challenge to signals

--- content/crutchcorn/collections/framework-field-guide-fundamentals/posts/ffg-fundamentals-derived-values/ffg-fundamentals-angular-derived-challenge-49/src/main.ts
+++ content/crutchcorn/collections/framework-field-guide-fundamentals/posts/ffg-fundamentals-derived-values/ffg-fundamentals-angular-derived-challenge-49/src/main.ts
@@ -1,35 +1,32 @@
 import "zone.js";
 import { bootstrapApplication } from "@angular/platform-browser";
 
-import { Component, Input, Pipe, PipeTransform } from "@angular/core";
+import { Component, computed, input } from "@angular/core";
 
-@Pipe({ name: "formatBytes", standalone: true })
-class FormatBytesPipe implements PipeTransform {
-	kilobyte = 1024;
-	megabyte = this.kilobyte * 1024;
-	gigabyte = this.megabyte * 1024;
+const kilobyte = 1024;
+const megabyte = kilobyte * 1024;
+const gigabyte = megabyte * 1024;
 
-	transform(bytes: number): string {
-		if (bytes < this.kilobyte) {
-			return `${bytes} B`;
-		} else if (bytes < this.megabyte) {
-			return `${Math.floor(bytes / this.kilobyte)} KB`;
-		} else if (bytes < this.gigabyte) {
-			return `${Math.floor(bytes / this.megabyte)} MB`;
-		} else {
-			return `${Math.floor(bytes / this.gigabyte)} GB`;
-		}
+function formatBytes(bytes: number) {
+	if (bytes < kilobyte) {
+		return `${bytes} B`;
+	} else if (bytes < megabyte) {
+		return `${Math.floor(bytes / kilobyte)} KB`;
+	} else if (bytes < gigabyte) {
+		return `${Math.floor(bytes / megabyte)} MB`;
+	} else {
+		return `${Math.floor(bytes / gigabyte)} GB`;
 	}
 }
 
 @Component({
 	selector: "display-size",
-	standalone: true,
-	imports: [FormatBytesPipe],
-	template: `<p>{{ bytes | formatBytes }}</p>`,
+	template: `<p>{{ readableBytes() }}</p>`,
 })
 class DisplaySizeComponent {
-	@Input() bytes!: number;
+	bytes = input.required<number>();
+
+	readableBytes = computed(() => formatBytes(this.bytes()));
 }
 
 @Component({
