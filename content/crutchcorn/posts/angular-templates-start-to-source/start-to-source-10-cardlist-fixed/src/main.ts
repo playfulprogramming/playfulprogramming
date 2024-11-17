4d920a922ba7cc4dea6094109f4f27d210c219ee
Corbin Crutchley <git@crutchcorn.dev>
2024-11-16 23:24:08 -0800

    docs: migrate sample 10 to Angular 18

--- content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-10-cardlist-fixed/src/main.ts
+++ content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-10-cardlist-fixed/src/main.ts
@@ -1,19 +1,75 @@
-import "./polyfills";
+import "zone.js";
+import { bootstrapApplication } from "@angular/platform-browser";
 
-import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import {
+	AfterViewInit,
+	Component,
+	ContentChildren,
+	ElementRef,
+	QueryList,
+	Renderer2,
+} from "@angular/core";
 
-import { AppModule } from "./app/app.module";
+@Component({
+	selector: "action-card",
+	standalone: true,
+	template: ` <div>This is a card</div> `,
+	styles: [
+		`
+			:host {
+				border: 1px solid black;
+				display: inline-block;
+				height: 300px;
+				width: 100px;
+				background: grey;
+				margin: 10px;
+			}
+		`,
+	],
+})
+export class ActionCard {}
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.then((ref) => {
-		// Ensure Angular destroys itself on hot reloads.
-		if (window["ngRef"]) {
-			window["ngRef"].destroy();
-		}
-		window["ngRef"] = ref;
+@Component({
+	selector: "cards-list",
+	standalone: true,
+	template: ` <div><ng-content></ng-content></div> `,
+	styles: [
+		`
+			:host {
+				background: grey;
+				display: block;
+			}
+		`,
+	],
+})
+export class CardsList implements AfterViewInit {
+	constructor(private renderer: Renderer2) {}
+	@ContentChildren(ActionCard, { read: ElementRef })
+	actionCards!: QueryList<ElementRef>;
 
-		// Otherwise, log the boot error
-	})
-	.catch((err) => console.error(err));
+	ngAfterViewInit() {
+		// Any production code should absolutely be running `Renderer2` to do this rather than modifying the native element yourself
+		this.actionCards.forEach((elRef) => {
+			console.log("Changing background of a card");
+			this.renderer.setStyle(elRef.nativeElement, "background", "white");
+		});
+	}
+}
+
+@Component({
+	selector: "my-app",
+	standalone: true,
+	imports: [ActionCard, CardsList],
+	template: `
+		<cards-list>
+			<!-- Cards list has default styling with grey background -->
+			<action-card></action-card>
+			<!-- Action card has default styling with grey background -->
+			<action-card></action-card>
+			<!-- It's also widely used across the app, so that can't change -->
+		</cards-list>
+	`,
+})
+export class AppComponent {}
+
+bootstrapApplication(AppComponent);
