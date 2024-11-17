1119fc7d30dd2de650055a828980c08d7cfce0f3
Corbin Crutchley <git@crutchcorn.dev>
2024-11-16 22:11:55 -0800

    docs: migrate first sample to Angular 18

--- content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-1-ng-template/src/main.ts
+++ content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-1-ng-template/src/main.ts
@@ -1,19 +1,22 @@
-import "./polyfills";
+import "zone.js";
+import { bootstrapApplication } from "@angular/platform-browser";
 
-import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { Component } from "@angular/core";
+import { NgIf } from "@angular/common";
 
-import { AppModule } from "./app/app.module";
+@Component({
+	selector: "my-app",
+	standalone: true,
+	imports: [NgIf],
+	template: `
+		<ng-template #falseTemp>
+			<p>False</p>
+		</ng-template>
+		<p *ngIf="bool; else falseTemp">True</p>
+	`,
+})
+export class AppComponent {
+	bool = false;
+}
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.then((ref) => {
-		// Ensure Angular destroys itself on hot reloads.
-		if (window["ngRef"]) {
-			window["ngRef"].destroy();
-		}
-		window["ngRef"] = ref;
-
-		// Otherwise, log the boot error
-	})
-	.catch((err) => console.error(err));
+bootstrapApplication(AppComponent);
