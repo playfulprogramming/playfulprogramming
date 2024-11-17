e48e93b553b4f9b9ca8d0ef0d6f622047a5c8e56
Corbin Crutchley <git@crutchcorn.dev>
2024-11-16 23:10:34 -0800

    docs: migrate sample 8 to Angular 18

--- content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-8-querylist/src/main.ts
+++ content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-8-querylist/src/main.ts
@@ -1,19 +1,56 @@
-import "./polyfills";
+import "zone.js";
+import { bootstrapApplication } from "@angular/platform-browser";
 
-import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import {
+	AfterViewInit,
+	Component,
+	Input,
+	QueryList,
+	ViewChildren,
+} from "@angular/core";
+import { NgIf } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { AppModule } from "./app/app.module";
+@Component({
+	selector: "my-custom-component",
+	standalone: true,
+	template: ` <p>My value is {{ numberProp }}</p> `,
+})
+export class MyComponentComponent {
+	@Input() inputHere!: number;
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.then((ref) => {
-		// Ensure Angular destroys itself on hot reloads.
-		if (window["ngRef"]) {
-			window["ngRef"].destroy();
-		}
-		window["ngRef"] = ref;
+	numberProp = Math.floor(Math.random() * 20);
+}
 
-		// Otherwise, log the boot error
-	})
-	.catch((err) => console.error(err));
+@Component({
+	selector: "my-app",
+	standalone: true,
+	imports: [MyComponentComponent, NgIf, FormsModule],
+	template: `
+		<p>The console should output the combination of all values below</p>
+		<input type="checkbox" [(ngModel)]="bool" />
+		<div *ngIf="bool">
+			<my-custom-component></my-custom-component>
+		</div>
+		<my-custom-component></my-custom-component>
+	`,
+})
+export class AppComponent implements AfterViewInit {
+	@ViewChildren(MyComponentComponent)
+	myComponents!: QueryList<MyComponentComponent>;
+
+	bool = false;
+
+	ngAfterViewInit() {
+		this.myComponents.changes.subscribe(
+			(compsQueryList: QueryList<MyComponentComponent>) => {
+				const componentsNum = compsQueryList.reduce((prev, comp) => {
+					return prev + comp.numberProp;
+				}, 0);
+				console.log(componentsNum); // This would output the combined number from all of the component's `numberProp` field. This would run any time Angular saw a difference in the values
+			},
+		);
+	}
+}
+
+bootstrapApplication(AppComponent);
