9ce80b17040267ce43dbea79b7e8de79d700f519
Corbin Crutchley <git@crutchcorn.dev>
2024-11-17 01:57:53 -0800

    docs: migrate 37 to Angular 18

--- content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-37-pig-latin-as-keyword-broken/src/main.ts
+++ content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-37-pig-latin-as-keyword-broken/src/main.ts
@@ -1,19 +1,75 @@
-import "./polyfills";
+import "zone.js";
+import { bootstrapApplication } from "@angular/platform-browser";
 
-import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import {
+	Component,
+	ViewContainerRef,
+	OnInit,
+	Input,
+	TemplateRef,
+	Directive,
+} from "@angular/core";
 
-import { AppModule } from "./app/app.module";
+import { UpperCasePipe } from "@angular/common";
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.then((ref) => {
-		// Ensure Angular destroys itself on hot reloads.
-		if (window["ngRef"]) {
-			window["ngRef"].destroy();
+@Directive({
+	selector: "[makePiglatin]",
+	standalone: true,
+})
+class MakePigLatinDirective implements OnInit {
+	constructor(
+		private templ: TemplateRef<any>,
+		private parentViewRef: ViewContainerRef,
+	) {}
+
+	@Input() makePiglatin!: string;
+	@Input() makePiglatinCasing!: string;
+
+	ngOnInit() {
+		let pigLatinVal = translatePigLatin(this.makePiglatin);
+		if (this.makePiglatinCasing === "UPPER") {
+			pigLatinVal = pigLatinVal.toUpperCase();
+		} else if (this.makePiglatinCasing === "lower") {
+			pigLatinVal = pigLatinVal.toLowerCase();
 		}
-		window["ngRef"] = ref;
+		this.parentViewRef.createEmbeddedView(this.templ, {
+			$implicit: pigLatinVal,
+			original: this.makePiglatin,
+		});
+	}
+}
 
-		// Otherwise, log the boot error
-	})
-	.catch((err) => console.error(err));
+@Component({
+	selector: "my-app",
+	standalone: true,
+	imports: [MakePigLatinDirective, UpperCasePipe],
+	template: `
+		<p
+			*makePiglatin="
+				'test';
+				let msg;
+				casing: 'upper' | uppercase as upperInUpper
+			"
+		>
+			{{ upperInUpper }}: {{ msg }}
+		</p>
+	`,
+})
+class AppComponent {}
+
+bootstrapApplication(AppComponent);
+
+function translatePigLatin(strr: string) {
+	// Code originally migrated from:
+	// https://www.freecodecamp.org/forum/t/freecodecamp-algorithm-challenge-guide-pig-latin/16039/7
+	if (!strr) return "";
+	return strr
+		.split(" ")
+		.map((str) => {
+			if (["a", "e", "i", "o", "u"].indexOf(str.charAt(0)) != -1) {
+				return (str += "way");
+			}
+			return str.replace(/([^aeiou]*)([aeiou]\w*)/, "$2$1ay");
+		})
+		.join(" ");
+}
