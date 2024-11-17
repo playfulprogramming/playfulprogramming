e86d3e2fd50da7b8590dd7160c983bcf934b352c
Corbin Crutchley <git@crutchcorn.dev>
2024-11-16 23:34:57 -0800

    docs: migrate sample 13 to Angular 18

--- content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-13-lifecycle-explain/src/main.ts
+++ content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-13-lifecycle-explain/src/main.ts
@@ -1,19 +1,54 @@
-import "./polyfills";
+import "zone.js";
+import { bootstrapApplication } from "@angular/platform-browser";
+import {
+	AfterViewInit,
+	Component,
+	DoCheck,
+	OnInit,
+	TemplateRef,
+	ViewChild,
+} from "@angular/core";
+import { NgTemplateOutlet } from "@angular/common";
 
-import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+@Component({
+	selector: "my-app",
+	standalone: true,
+	imports: [NgTemplateOutlet],
+	template: `
+		<ng-template #helloThereMsg>
+			Hello There!
+			<ng-template #testingMessage>Testing 123</ng-template>
+		</ng-template>
+		<ng-template [ngTemplateOutlet]="helloThereMsg"></ng-template>
+		<ng-template [ngTemplateOutlet]="realMsgVar"></ng-template>
+	`,
+})
+export class AppComponent implements OnInit, DoCheck, AfterViewInit {
+	realMsgVar!: TemplateRef<any>;
+	@ViewChild("testingMessage", { static: false })
+	testingMessageCompVar!: TemplateRef<any>;
 
-import { AppModule } from "./app/app.module";
+	ngOnInit() {
+		console.log(
+			"ngOnInit: The template is present?",
+			!!this.testingMessageCompVar,
+		);
+	}
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.then((ref) => {
-		// Ensure Angular destroys itself on hot reloads.
-		if (window["ngRef"]) {
-			window["ngRef"].destroy();
-		}
-		window["ngRef"] = ref;
+	ngDoCheck() {
+		console.log(
+			"ngDoCheck: The template is present?",
+			!!this.testingMessageCompVar,
+		);
+		this.realMsgVar = this.testingMessageCompVar;
+	}
 
-		// Otherwise, log the boot error
-	})
-	.catch((err) => console.error(err));
+	ngAfterViewInit() {
+		console.log(
+			"ngAfterViewInit: The template is present?",
+			!!this.testingMessageCompVar,
+		);
+	}
+}
+
+bootstrapApplication(AppComponent);
