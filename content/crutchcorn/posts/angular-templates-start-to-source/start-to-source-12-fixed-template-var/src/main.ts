010a6c806aeaa0e8aa350e3f71840d9a13feda86
Corbin Crutchley <git@crutchcorn.dev>
2024-11-16 23:31:24 -0800

    docs: migrate 12 sample to Angular 18

--- content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-12-fixed-template-var/src/main.ts
+++ content/crutchcorn/posts/angular-templates-start-to-source/start-to-source-12-fixed-template-var/src/main.ts
@@ -1,19 +1,22 @@
-import "./polyfills";
+import "zone.js";
+import { bootstrapApplication } from "@angular/platform-browser";
 
-import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { Component } from "@angular/core";
+import { NgTemplateOutlet } from "@angular/common";
 
-import { AppModule } from "./app/app.module";
+@Component({
+	selector: "my-app",
+	standalone: true,
+	imports: [NgTemplateOutlet],
+	template: `
+		<ng-template #helloThereMsg>
+			Hello There!
+			<ng-template #testingMessage><p>Testing 123</p></ng-template>
+			<ng-template [ngTemplateOutlet]="testingMessage"></ng-template>
+		</ng-template>
+		<ng-template [ngTemplateOutlet]="helloThereMsg"></ng-template>
+	`,
+})
+export class AppComponent {}
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.then((ref) => {
-		// Ensure Angular destroys itself on hot reloads.
-		if (window["ngRef"]) {
-			window["ngRef"].destroy();
-		}
-		window["ngRef"] = ref;
-
-		// Otherwise, log the boot error
-	})
-	.catch((err) => console.error(err));
+bootstrapApplication(AppComponent);
