0c0e90c344f5a0246479bcf26d35ef122bf8b4e6
James Fenn <me@jfenn.me>
2024-12-01 11:32:30 -0500

    separate link preview styling from anchored tooltips, update tokens

--- src/utils/markdown/components/link-preview/link-preview.tsx
+++ src/utils/markdown/components/link-preview/link-preview.tsx
@@ -22,8 +22,8 @@ interface LinkPreviewProps {
 /** @jsxImportSource hastscript */
 export function LinkPreview(props: LinkPreviewProps): Element {
 	return (
-		<a class={`anchored-tooltip anchored-tooltip--${props.type}`} {...props.anchorAttrs}>
-			<span class="anchored-tooltip__label text-style-button-regular">
+		<a class={`link-preview`} {...props.anchorAttrs}>
+			<span class={`anchored-tooltip anchored-tooltip--${props.type}`}>
 				{props.type === "zoom" ? FullscreenIcon : LaunchIcon}
 				<span>{props.label}</span>
 			</span>
