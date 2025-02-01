1dad77911b5a446c66ff8f2c7eede0cd7c4740b0
Corbin Crutchley <git@crutchcorn.dev>
2025-02-01 06:57:27 -0800

    docs: fix types in Vue example

--- content/crutchcorn/collections/art-of-accessibility/posts/art-of-a11y-text/art-of-a11y-vue-as-cast/src/Header.vue
+++ content/crutchcorn/collections/art-of-accessibility/posts/art-of-a11y-text/art-of-a11y-vue-as-cast/src/Header.vue
@@ -1,8 +1,10 @@
 <!-- Header.vue -->
 <script setup lang="ts" generic="T extends keyof HTMLElementTagNameMap = 'h1'">
-const props = defineProps<{
-	as?: T;
-}>();
+const props = defineProps<
+	Partial<HTMLElementTagNameMap[T]> & {
+		as?: T;
+	}
+>();
 
 const Component = props.as || "h1";
 </script>
