8ddd65eff522ab8c6708261aa3c18900dfcef1d9
Courtney Hackshaw <ckhackshaw@gmail.com>
2025-02-10 00:21:00 -0400

    set up styles for ArticleRevisionDropdown component

--- src/views/blog-post/article-revisions/article-revisions.tsx
+++ src/views/blog-post/article-revisions/article-revisions.tsx
@@ -1,10 +1,16 @@
+import style from "./article-revisions.module.scss";
 import { Select, Item } from "../../../components/select/select";
 
 export function ArticleRevisionDropdown() {
 	return (
 		<div>
 			{/* <!-- <p class="text-style-button-regular">{publishedStr}</p> --> */}
-			<Select defaultValue="one" prefixSelected="test: ">
+			<Select
+				defaultValue="One"
+				prefixSelected=""
+				testId={"article-revision-selection"}
+				class={style.test}
+			>
 				<Item key={"one"}>One</Item>
 				<Item key={"two"}>Two</Item>
 				<Item key={"three"}>Three</Item>
