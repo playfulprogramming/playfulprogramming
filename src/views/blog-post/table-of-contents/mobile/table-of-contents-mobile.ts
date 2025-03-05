514cc5ab8a5adf5e78bf5c2007b5d191aaf156d1
josephmfaulkner <13591699+josephmfaulkner@users.noreply.github.com>
2025-03-04 19:32:17 -0800

    Mobile Table of Contents: handle menu item click

---
+++ src/views/blog-post/table-of-contents/mobile/table-of-contents-mobile.ts
@@ -0,0 +1,70 @@
+import styles from "./table-of-contents-mobile.module.scss";
+
+export const setupMobileTableOfContentsToggle = () => {
+	const mainBody = document.getElementsByTagName("body")[0];
+	const mainPageContents = document.querySelector<HTMLElement>(
+		"[data-article-page-contents]",
+	);
+
+	const mobileTableOfContentsMenu = document.querySelector<HTMLElement>(
+		"[data-mobile-table-of-contents-menu]",
+	);
+	const mobileTableOfContentsMenuHeaderText =
+		document.querySelector<HTMLElement>(
+			"[data-mobile-table-of-contents-menu-header]",
+		);
+	const mobileTableOfContentsMenuList = document.querySelector<HTMLElement>(
+		"[data-mobile-table-of-contents-menu-list]",
+	);
+
+	const mobileTableOfContentsMenuToggleButton =
+		document.querySelector<HTMLButtonElement>(
+			"[data-mobile-table-of-contents-toggle-button]",
+		);
+
+	const mobileTableOfContentsMenuListItems =
+		mobileTableOfContentsMenuList?.querySelectorAll<HTMLLinkElement>("a");
+
+	const _toggleMobileMenuStyles = () => {
+		mobileTableOfContentsMenu?.classList.toggle(styles.tocMobileMenuCollapsed);
+		mainBody?.classList.toggle(styles.tocMobileOpenMainBody);
+	};
+
+	const _toggleMobileMenuInert = () => {
+		const mobileMenuCollapsed = mobileTableOfContentsMenu?.classList.contains(
+			styles.tocMobileMenuCollapsed,
+		);
+
+		if (mainPageContents) {
+			mainPageContents.inert = !mobileMenuCollapsed;
+		}
+		if (mobileTableOfContentsMenuList) {
+			mobileTableOfContentsMenuList.inert = mobileMenuCollapsed || false;
+		}
+	};
+
+	const toggleMenuExpanded = () => {
+		_toggleMobileMenuStyles();
+		_toggleMobileMenuInert();
+	};
+
+	const setMenuHeaderText = (newText: string) => {
+		if (mobileTableOfContentsMenuHeaderText) {
+			mobileTableOfContentsMenuHeaderText.innerText = newText;
+		}
+	};
+
+	const handleMenuItemClick = (event) => {
+		toggleMenuExpanded();
+		setMenuHeaderText(event.target.textContent);
+	};
+
+	mobileTableOfContentsMenuToggleButton?.addEventListener(
+		"click",
+		toggleMenuExpanded,
+	);
+
+	mobileTableOfContentsMenuListItems?.forEach((menuItem) => {
+		menuItem.addEventListener("click", handleMenuItemClick);
+	});
+};
