5df72ee7e654e21fb1bd619ed03558782ebc7dad
James Fenn <me@jfenn.me>
2025-05-18 11:42:44 -0400

    add error handling for link-preview

--- src/utils/hoof/get-url-metadata.ts
+++ src/utils/hoof/get-url-metadata.ts
@@ -1,4 +1,3 @@
-import { setTimeout } from "node:timers/promises";
 import { client } from "./client";
 
 interface UrlMetadataResponse {
@@ -18,27 +17,14 @@ interface UrlMetadataResponse {
 export async function getUrlMetadata(
 	url: string,
 ): Promise<UrlMetadataResponse> {
-	for (let retries = 0; retries < 10; retries++) {
-		await setTimeout(Math.pow(retries, 2) * 1000);
+	const { data, response } = await client.POST("/tasks/url-metadata", {
+		body: { url },
+	});
 
-		const { data, response } = await client.POST("/tasks/url-metadata", {
-			body: { url },
-		});
-
-		if (data && response.status === 200) {
-			return data;
-		}
-
-		if (response.status !== 202) {
-			const error = `Error ${response.status} fetching url: '${url}'`;
-			console.error(error);
-			throw new Error(error);
-		}
-
-		if (retries > 2) {
-			console.warn(`Waiting for URL metadata (retry ${retries})`, { url });
-		}
+	if (data && response.status === 200) {
+		return data;
 	}
 
-	throw Error(`Waited too long for url metadata: '${url}'`);
+	const error = `Error ${response.status} fetching url: '${url}'`;
+	throw new Error(error);
 }
