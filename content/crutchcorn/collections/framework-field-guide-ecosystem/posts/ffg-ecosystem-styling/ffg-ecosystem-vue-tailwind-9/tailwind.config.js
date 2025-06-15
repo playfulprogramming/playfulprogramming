23ab40a6a9e0382838dc2a1c040091dee2326c89
Corbin Crutchley <git@crutchcorn.dev>
2025-06-15 06:36:24 -0700

    docs: update Tailwind usage on all FWs

--- content/crutchcorn/collections/framework-field-guide-ecosystem/posts/ffg-ecosystem-styling/ffg-ecosystem-vue-tailwind-9/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html,vue}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
--- content/crutchcorn/collections/framework-field-guide-ecosystem/posts/ffg-ecosystem-styling/ffg-ecosystem-vue-tailwind-9/vite.config.js
+++ content/crutchcorn/collections/framework-field-guide-ecosystem/posts/ffg-ecosystem-styling/ffg-ecosystem-vue-tailwind-9/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), tailwindcss()],
 });
