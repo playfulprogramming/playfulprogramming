af00d877b4e7004939f5b3e65d0188913ed42ba0
Corbin Crutchley <git@crutchcorn.dev>
2025-06-15 05:54:27 -0700

    docs: upgrade Bundler chapter deps

--- content/crutchcorn/collections/framework-field-guide-ecosystem/posts/ffg-ecosystem-bundling/ffg-ecosystem-angular-build-5/README.md
+++ content/crutchcorn/collections/framework-field-guide-ecosystem/posts/ffg-ecosystem-bundling/ffg-ecosystem-angular-build-5/README.md
@@ -1,6 +1,6 @@
 # FfgEcosystemAngularBuild5
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.0.0-next.11.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.2.
 
 ## Development server
 
--- content/crutchcorn/collections/framework-field-guide-ecosystem/posts/ffg-ecosystem-bundling/ffg-ecosystem-angular-build-5/src/app/app.config.ts
+++ content/crutchcorn/collections/framework-field-guide-ecosystem/posts/ffg-ecosystem-bundling/ffg-ecosystem-angular-build-5/src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(routes),
-	],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes)
+  ]
 };
