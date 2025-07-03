f8ffea124bb9f294e7f84bba43badb2c2d056d4c
Courtney Hackshaw <ckhackshaw@gmail.com>
2025-07-03 12:15:31 -0400

    change to a dumb component

--- src/components/select/basic-option.tsx
+++ src/components/select/basic-option.tsx
@@ -1,21 +1,9 @@
 import { ComponentChildren } from "preact";
-import styles from "./select.module.scss";
 
 interface OptionProps {
-	text: ComponentChildren;
-	isSelected?: boolean;
-	icon: ComponentChildren;
+	children: ComponentChildren;
 }
 
-export function Option({ text, isSelected = false, icon }: OptionProps) {
-	console.log("isSelected: ", isSelected);
-
-	return (
-		<li class={`${styles.option} ${isSelected ? styles.selected : ""}`}>
-			<span class={`text-style-button-regular ${styles.optionText}`}>
-				{text}
-			</span>
-			<span className={styles.checkmark}>{icon}</span>
-		</li>
-	);
+export function Option({ children }: OptionProps) {
+	return <li>{children}</li>;
 }
