9df8ecf84f514d5dddc87c72c47d0b5581ad9c6d
Corbin Crutchley <git@crutchcorn.dev>
2025-07-25 06:21:10 -0700

    docs: various fixes to the styling chapter

--- content/crutchcorn/collections/framework-field-guide-ecosystem/posts/ffg-ecosystem-styling/ffg-ecosystem-react-sass-13/src/main.jsx
+++ content/crutchcorn/collections/framework-field-guide-ecosystem/posts/ffg-ecosystem-styling/ffg-ecosystem-react-sass-13/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.jsx";
+import { App } from "./app.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
