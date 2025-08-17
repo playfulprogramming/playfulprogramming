164aef9ec6eceb9a1fb78f7d3990e8ce44b6f134
mikecebul <mikecebul@gmail.com>
2025-08-16 22:36:34 -0400

    fix: update repoPath to use environment variable and add Hoof URL to dev environment

--- src/constants/site-config.ts
+++ src/constants/site-config.ts
@@ -33,7 +33,7 @@ export const siteMetadata = {
 	title: `Playful Programming`,
 	description: `Learning programming from magically majestic words. A place to learn about all sorts of programming topics from entry-level concepts to advanced abstractions`,
 	siteUrl,
-	repoPath: "playfulprogramming/playfulprogramming",
+	repoPath: env("REPO_PATH") || "playfulprogramming/playfulprogramming",
 	relativeToPosts: "/content/blog",
 	keywords:
 		"programming,development,mobile,web,game,playful,software engineering,javascript,angular,react,computer science",
