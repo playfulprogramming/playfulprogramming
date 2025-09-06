9e790d2e3c063484d221cecfed50acb00590c530
James Fenn <me@jfenn.me>
2025-09-06 17:20:54 -0400

    document env/index.ts behavior

--- src/constants/env/index.ts
+++ src/constants/env/index.ts
@@ -1,19 +1,22 @@
 import { Environment } from "./types";
 
-let isNode = false;
+let isAstro: boolean;
 try {
-	// This will throw an error if run on node, as import.meta.env is undefined
-	// (when run through Astro, this entire statement will be statically replaced by Vite)
+	// This will throw an error if run on node without Vite, as import.meta.env is undefined
+	// (when run through Astro, this entire statement will be statically replaced)
 	String(import.meta.env.MODE);
+	isAstro = true;
 } catch (_) {
-	isNode = true;
+	isAstro = false;
 }
 
+// Environment must either use import.meta.env or process.env depending on where/how this is run
+// Since Vite replaces import.meta.env replaces, runtime logic is not possible here
 let env: Environment;
-if (isNode) {
-	env = (await import("./env-node")).default;
-} else {
+if (isAstro) {
 	env = (await import("./env-astro")).default;
+} else {
+	env = (await import("./env-node")).default;
 }
 
 export default env;
