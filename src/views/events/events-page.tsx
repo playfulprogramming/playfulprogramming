36ace8242a0fe60eeb3c244346697385b13748bc
Corbin Crutchley <git@crutchcorn.dev>
2025-11-01 09:12:50 -0700

    chore: fix styling for events header

--- src/views/events/events-page.tsx
+++ src/views/events/events-page.tsx
@@ -4,7 +4,10 @@ import {
 	RadioButton,
 	RadioButtonGroup,
 } from "components/button-radio-group/button-radio-group";
-import styles from "./events-page.module.scss";
+import { IconOnlyButton } from "components/button/button";
+
+import filter from "src/icons/filter.svg?raw";
+import style from "./events-page.module.scss";
 
 type EventType = "all" | "online" | "in-person";
 
@@ -30,29 +33,36 @@ export default function SearchPageBase() {
 	}, [filteredEvents]);
 
 	return (
-		<div className={styles.container}>
-			<div className={styles.titleContainer}>
-				<h1 className={`text-style-headline-1 ${styles.eventsTitle}`}>
-					Events
-				</h1>
-				<RadioButtonGroup
-					className={styles.eventTypesToShowGroup}
-					value={eventTypesToShow}
-					label={"Show:"}
-					onChange={(val) => setEventTypesToShow(val as EventType)}
-				>
-					<RadioButton value={"all"}>All Events</RadioButton>
-					<RadioButton value={"online"}>Online</RadioButton>
-					<RadioButton value={"in-person"}>In-person</RadioButton>
-				</RadioButtonGroup>
+		<div className={style.container}>
+			<div className={style.titleContainer}>
+				<h1 className={`text-style-headline-1 ${style.eventsTitle}`}>Events</h1>
+				<div className={style.showButtonContainer}>
+					<div className={style.showTextContainer}>
+					<span
+						className={style.filterIconContainer}
+						dangerouslySetInnerHTML={{ __html: filter }}
+					></span>
+					<span className={`text-style-button-regular`}>Show:</span>
+					</div>
+					<RadioButtonGroup
+						className={style.eventTypesToShowGroup}
+						value={eventTypesToShow}
+						label={"Show:"}
+						onChange={(val) => setEventTypesToShow(val as EventType)}
+					>
+						<RadioButton value={"all"}>All Events</RadioButton>
+						<RadioButton value={"online"}>Online</RadioButton>
+						<RadioButton value={"in-person"}>In-person</RadioButton>
+					</RadioButtonGroup>
+				</div>
 			</div>
-			<div className={styles.listsContainer}>
+			<div className={style.listsContainer}>
 				{recurringEvents.length ? (
-					<div className={styles.listContainer}>
-						<h2 className={`text-style-headline-5 ${styles.listHeading}`}>
+					<div className={style.listContainer}>
+						<h2 className={`text-style-headline-5 ${style.listHeading}`}>
 							Recurring events
 						</h2>
-						<ul className={styles.list}>
+						<ul className={style.list}>
 							{recurringEvents.map((event) => (
 								<li key={event.slug}>{event.title}</li>
 							))}
@@ -60,11 +70,11 @@ export default function SearchPageBase() {
 					</div>
 				) : null}
 				{nonRecurringEvents.length ? (
-					<div className={styles.listContainer}>
-						<h2 className={`text-style-headline-5 ${styles.listHeading}`}>
+					<div className={style.listContainer}>
+						<h2 className={`text-style-headline-5 ${style.listHeading}`}>
 							Special events
 						</h2>
-						<ul className={styles.list}>
+						<ul className={style.list}>
 							{nonRecurringEvents.map((event) => (
 								<li key={event.slug}>{event.title}</li>
 							))}
