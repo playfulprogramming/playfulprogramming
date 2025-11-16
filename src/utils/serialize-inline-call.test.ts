bee4613709794e7a50dc614f419fd49f2544ecea
Jacob Asper <jacobasper191@gmail.com>
2025-11-16 18:38:51 -0500

    type safely serialize function instead of string replacement

---
+++ src/utils/serialize-inline-call.test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from "vitest";
+
+import { serializeInlineCall } from "./serialize-inline-call";
+
+const sampleFunction = (
+	title: string,
+	count: number,
+	config: { wooperMode: boolean },
+) => {
+	console.log("rust is a must 🦀🦀🦀🦀🦀");
+	return { title, count, config };
+};
+
+test("serializes a function with arguments", () => {
+	const inline = serializeInlineCall(sampleFunction)("rust", 8923988932893893, {
+		wooperMode: true,
+	});
+
+	expect(inline).toMatchInlineSnapshot(`
+			"((title, count, config) => {
+			  console.log("rust is a must 🦀🦀🦀🦀🦀");
+			  return { title, count, config };
+			})("rust",8923988932893893,{"wooperMode":true});"
+		`);
+});
