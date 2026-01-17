83cb1220ff97bb3d6e9bd946906b8e75b4e01c5c
Corbin Crutchley <git@crutchcorn.dev>
2026-01-17 07:20:28 -0800

    fix: only embed X posts

--- src/utils/markdown/iframes/platform-detectors/x.ts
+++ src/utils/markdown/iframes/platform-detectors/x.ts
@@ -6,41 +6,41 @@ export const xPlatformDetector: PlatformDetector = {
 	detect: (src) => {
 		const srcUrl = new URL(src);
 		const isX = xHosts.includes(srcUrl.hostname);
-		return isX;
-	},
-	rehypeTransform: async ({ parent, index, src }) => {
-		const srcUrl = new URL(src);
 
 		// https://x.com/playful_program/status/1917675872854614490
 		const xPathParts = srcUrl.pathname.split("/").filter(Boolean);
 		const xStatus = xPathParts[1];
 		const isXPost = xStatus === "status";
-		if (isXPost) {
-			const xUserId = xPathParts[0];
-			const xPostId = xPathParts[xPathParts.length - 1];
 
-			const post = await getXPostData({
-				userId: xUserId,
-				postId: xPostId,
-			});
+		return isX && isXPost;
+	},
+	rehypeTransform: async ({ parent, index, src }) => {
+		const srcUrl = new URL(src);
+		const xPathParts = srcUrl.pathname.split("/").filter(Boolean);
+		const xUserId = xPathParts[0];
+		const xPostId = xPathParts[xPathParts.length - 1];
 
-			if (!post) {
-				// TODO: Handle 404 properly
-				return;
-			}
+		const post = await getXPostData({
+			userId: xUserId,
+			postId: xPostId,
+		});
 
-			parent.children.splice(
-				index,
-				1,
-				createComponent("XPlaceholder", {
-					text: post.text,
-					profilePic: post.author.avatar_url,
-					likes: post.likes,
-					reposts: post.reposts,
-					replies: post.replies,
-					// TODO: Handle video, images, et al
-				}),
-			);
+		if (!post) {
+			// TODO: Handle 404 properly
+			return;
 		}
+
+		parent.children.splice(
+			index,
+			1,
+			createComponent("XPlaceholder", {
+				text: post.text,
+				profilePic: post.author.avatar_url,
+				likes: post.likes,
+				reposts: post.reposts,
+				replies: post.replies,
+				// TODO: Handle video, images, et al
+			}),
+		);
 	},
 };
