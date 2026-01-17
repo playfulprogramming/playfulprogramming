45bcb93a5bc760f29f313d32800761ec9151486c
Corbin Crutchley <git@crutchcorn.dev>
2026-01-17 07:53:52 -0800

    feat: add Shiki to embed system

---
+++ src/utils/markdown/iframes/hastscript-components/GistCodeBlock.tsx
@@ -0,0 +1,18 @@
+/** @jsxRuntime automatic */
+import type { Element } from "hast";
+
+interface GistCodeBlockProps {
+	language: string;
+	contents: string;
+	truncated: boolean;
+}
+
+/** @jsxImportSource hastscript */
+export function GistCodeBlock(props: GistCodeBlockProps): Element {
+	// https://github.com/leafac/rehype-shiki?tab=readme-ov-file#format
+	return (
+		<pre>
+			<code class={`language-${props.language}`}>{props.contents}</code>
+		</pre>
+	) as never;
+}
--- src/utils/markdown/iframes/platform-detectors/gist.ts
+++ src/utils/markdown/iframes/platform-detectors/gist.ts
@@ -2,6 +2,7 @@ import { PlatformDetector } from "utils/markdown/iframes/platform-detectors/type
 import { getGist } from "utils/markdown/data-providers";
 import { createComponent } from "utils/markdown/components";
 import { gistHosts } from "utils/markdown/data-providers/gist";
+import { GistCodeBlock } from "utils/markdown/iframes/hastscript-components/GistCodeBlock";
 
 export const gistPlatformDetector: PlatformDetector = {
 	detect: (src) => {
@@ -14,15 +15,60 @@ export const gistPlatformDetector: PlatformDetector = {
 
 		// https://gist.github.com/crutchcorn/36fe5553219c05ea38bacf1c7396085b
 		const gistPathParts = srcUrl.pathname.split("/").filter(Boolean);
-		const _githubUsername = gistPathParts[0];
+		const githubUsername = gistPathParts[0];
 		const gistId = gistPathParts[1];
 
 		const data = await getGist(gistId);
 
+		// TODO: How to handle no files in a Gist?
+		if (!data.files) return;
+
+		const file = Object.values(
+			// "File name.txt": {}
+			data.files,
+		)[0];
+
+		if (!file) return;
+		if (!file.content) return;
+
+		const { contents, truncated } = limitStringToNLines(file.content, 10);
+		// This is the language from GitHub's API, might not align with Shiki's lang code
+		const language = file.language?.toLowerCase() ?? "text";
+
 		parent.children.splice(
 			index,
 			1,
-			createComponent("GistPlaceholder", { data }),
+			createComponent(
+				"GistPlaceholder",
+				{
+					username: githubUsername
+				},
+				[
+					GistCodeBlock({
+						language,
+						contents,
+						truncated,
+					}),
+				],
+			),
 		);
 	},
 };
+
+function limitStringToNLines(longString: string, numberOfLines: number) {
+	// Split the string into an array of lines based on newline characters
+	const lines = longString.split("\n");
+
+	// If the number of lines is already 30 or less, return the original string
+	if (lines.length <= 30) {
+		return { contents: longString, truncated: false };
+	}
+
+	// Slice the array to get only the first 30 lines
+	const limitedLines = lines.slice(0, 30);
+
+	// Join the limited lines back into a single string with newline characters
+	const result = limitedLines.join("\n");
+
+	return { contents: result, truncated: true };
+}
