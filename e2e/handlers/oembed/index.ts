819edd65d7952d16aa9b70abd291b254b4e963f8
Corbin Crutchley <git@crutchcorn.dev>
2026-01-21 17:19:49 -0800

    chore: add X handler for E2E tests

--- e2e/handlers/oembed/index.ts
+++ e2e/handlers/oembed/index.ts
@@ -1,4 +1,5 @@
 import { vimeoHandlers } from "./vimeo";
 import { youtubeHandlers } from "./youtube";
+import { xHandlers } from "./x";
 
-export const handlers = [...vimeoHandlers, ...youtubeHandlers];
+export const handlers = [...vimeoHandlers, ...youtubeHandlers, ...xHandlers];
