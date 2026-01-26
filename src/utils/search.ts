e946b208b0104952366a6b0cb33e2db3efc57a21
Corbin Crutchley <git@crutchcorn.dev>
2026-01-25 23:11:21 -0800

    chore: fix one failure

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -1,9 +1,21 @@
 import Typesense from "typesense";
+import {
+	PUBLIC_SEARCH_ENDPOINT_HOST,
+	PUBLIC_SEARCH_ENDPOINT_PORT,
+	PUBLIC_SEARCH_ENDPOINT_PROTOCOL,
+	PUBLIC_SEARCH_KEY,
+} from "../views/search/constants";
 
 // Don't need this value at runtime, only there to get type information
 const client = new Typesense.Client({
-	nodes: [],
-	apiKey: "",
+	nodes: [
+		{
+			host: PUBLIC_SEARCH_ENDPOINT_HOST,
+			port: PUBLIC_SEARCH_ENDPOINT_PORT,
+			protocol: PUBLIC_SEARCH_ENDPOINT_PROTOCOL,
+		},
+	],
+	apiKey: PUBLIC_SEARCH_KEY,
 });
 const collection = client.collections();
 type CreateSchema = Parameters<(typeof collection)["create"]>[0];
