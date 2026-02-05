f4a8e61a91482fd4152e6a22c74d730df0588b3b
Corbin Crutchley <git@crutchcorn.dev>
2026-02-04 16:35:38 -0800

    chore: more resiliently handle 404s

--- src/utils/markdown/data-providers/gist.ts
+++ src/utils/markdown/data-providers/gist.ts
@@ -3,19 +3,23 @@ import { Octokit } from "octokit";
 const octokit = new Octokit();
 
 export async function getGist(gistId: string) {
-	const res = await octokit.request("GET /gists/{gist_id}", {
-		gist_id: gistId,
-		headers: {
-			accept: "application/vnd.github+json",
-			"X-GitHub-Api-Version": "2022-11-28",
-		},
-	});
+	try {
+		const res = await octokit.request("GET /gists/{gist_id}", {
+			gist_id: gistId,
+			headers: {
+				accept: "application/vnd.github+json",
+				"X-GitHub-Api-Version": "2022-11-28",
+			},
+		});
 
-	if (res.status !== 200) {
+		if (res.status !== 200) {
+			return null;
+		}
+
+		return res.data;
+	} catch (e) {
 		return null;
 	}
-
-	return res.data;
 }
 
 export const gistHosts = ["gist.github.com"];
