704d15a4482bf2324ba1f0e2f0ade538d11f5f1e
Corbin Crutchley <git@crutchcorn.dev>
2026-02-09 03:12:23 -0800

    chore: make e2e tests more resiliant against article intros

--- src/utils/markdown/components/user/rehype-transform.ts
+++ src/utils/markdown/components/user/rehype-transform.ts
@@ -8,6 +8,12 @@ export const transformUser: RehypeFunctionComponent = ({ attributes }) => {
 
 	const author = getPersonById(user, "en");
 
+	// Prevent author count increasing from breaking e2e tests
+	if (process.env.USE_E2E_MOCKS && author) {
+		author.totalPostCount = 100;
+		author.totalWordCount = 10000;
+	}
+
 	if (!author) return;
 
 	return [
