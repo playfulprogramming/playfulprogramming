ba1aa3adcbe00e4d233c0451c05398fd1f14f733
James Fenn <me@jfenn.me>
2026-02-16 20:09:03 -0500

    react aria

--- src/components/quiz-radio/quiz-radio.tsx
+++ src/components/quiz-radio/quiz-radio.tsx
@@ -1,8 +1,11 @@
 import CheckmarkIcon from "src/icons/checkmark.svg?raw";
 import DotIcon from "src/icons/dot.svg?raw";
 import CloseIcon from "src/icons/close.svg?raw";
-import { useId } from "preact/hooks";
+import { useId, useState } from "preact/hooks";
 import { RawSvg } from "components/image/raw-svg";
+import style from "./quiz-radio.module.scss";
+import { Button } from "components/button/button";
+import { Form, Label, Radio, RadioGroup } from "react-aria-components";
 
 export interface QuizRadioOption {
 	id: string;
@@ -21,82 +24,56 @@ export interface QuizRadioProps {
 export function QuizRadio(props: QuizRadioProps): Element {
 	const { title, options } = props;
 	const quizId = useId();
+	const [selectedOption, setSelectedOption] = useState();
 
 	return (
-		<form data-quiz-radio class="quizOptionContainer">
-			<fieldset>
-				<div class="quizOptionTitle">
-					<legend class="text-style-headline-6">{title}</legend>
-				</div>
-
-				<div class="quizOptionOptionsContainer">
+		<Form className={style.quizOptionContainer}>
+			<RadioGroup>
+				<Label>{title}</Label>
+				<div class={style.quizOptionOptionsContainer}>
 					{options.map((option) => {
 						const optionId = `quiz-option:${quizId}:${option.id}`;
 						return (
-							<div class="quizOptionOptionContainer" key={optionId}>
-								<label>
-									<input
-										class="quizOptionOptionInput"
-										required
-										type="radio"
-										name={optionId}
-										value={option.id}
-									/>
-									<div class="quizOptionOptionInnerContainer">
-										<div class="quizOptionOptionCheckboxSvgContainer">
-											<RawSvg
-												class="quizOptionOptionCheckboxCheckmarkSvgContainer"
-												icon={CheckmarkIcon}
-											/>
-											<RawSvg
-												class="quizOptionOptionCheckboxDotSvgContainer"
-												icon={DotIcon}
-											/>
-											<RawSvg
-												class="quizOptionOptionCheckboxCloseSvgContainer"
-												icon={CloseIcon}
-											/>
-										</div>
-										{option.label}
-									</div>
-								</label>
-							</div>
+							<Radio
+								key={optionId}
+								value={optionId}
+								className={style.quizOptionOptionInnerContainer}
+							>
+								{option.label}
+							</Radio>
 						);
 					})}
 				</div>
-			</fieldset>
+			</RadioGroup>
 
-			<div class="quizOptionBottomContainer">
-				<div class="quizOptionButtonRowContainer">
+			<div class={style.quizOptionBottomContainer}>
+				<div class={style.quizOptionButtonRowContainer}>
 					<p
 						style="display: none"
 						data-loading-message
-						class="quizOptionLoading text-style-button-regular"
+						class={`${style.quizOptionLoading} text-style-button-regular`}
 					>
-						Submitting your answer<span class="quizOptionLoadingDotOne">.</span>
-						<span class="quizOptionLoadingDotTwo">.</span>
-						<span class="quizOptionLoadingDotThree">.</span>
+						Submitting your answer
+						<span class={style.quizOptionLoadingDotOne}>.</span>
+						<span class={style.quizOptionLoadingDotTwo}>.</span>
+						<span class={style.quizOptionLoadingDotThree}>.</span>
 					</p>
-					<p data-votes class="quizOptionVotes">
+					<p data-votes class={style.quizOptionVotes}>
 						{props.votesLabel}
 					</p>
-					<span class="quizOptionButtonContainer">
-						<button
-							disabled
-							class="button text-style-button-regular primary-emphasized regular"
-						>
-							<span className="innerText">Submit</span>
-						</button>
+					<span class={style.quizOptionButtonContainer}>
+						<Button tag="button" variant="primary-emphasized" disabled>
+							Submit
+						</Button>
 					</span>
 				</div>
 				<p
 					style="display: none"
-					data-error-message
-					class="quizOptionErrorCode text-style-body-small-bold"
+					class={`${style.quizOptionErrorCode} text-style-body-small-bold`}
 				>
 					An error occurred. Please try again.
 				</p>
 			</div>
-		</form>
+		</Form>
 	) as never;
 }
