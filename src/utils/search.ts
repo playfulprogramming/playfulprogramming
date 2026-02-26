12ef3da3877fb9a614a0b0d09b932509ef5474e9
Furkan Emin Can <78358128+femincan@users.noreply.github.com>
2026-02-26 23:47:00 +0300

    refactor: replace client side logic with compile time type

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -1,24 +1,24 @@
-import Typesense from "typesense";
-import {
-	PUBLIC_SEARCH_ENDPOINT_HOST,
-	PUBLIC_SEARCH_ENDPOINT_PORT,
-	PUBLIC_SEARCH_ENDPOINT_PROTOCOL,
-	PUBLIC_SEARCH_KEY,
-} from "../views/search/constants";
+import { Client } from "typesense";
 
-// Don't need this value at runtime, only there to get type information
-const client = new Typesense.Client({
-	nodes: [
-		{
-			host: PUBLIC_SEARCH_ENDPOINT_HOST,
-			port: PUBLIC_SEARCH_ENDPOINT_PORT,
-			protocol: PUBLIC_SEARCH_ENDPOINT_PROTOCOL,
-		},
-	],
-	apiKey: PUBLIC_SEARCH_KEY,
-});
-const collection = client.collections();
-type CreateSchema = Parameters<(typeof collection)["create"]>[0];
+type CollectionsFunction = Client["collections"];
+// This type is for getting the expected overload signature by argument types from collections function overload. It may break in a future update of the library if more overload signatures added for collections function because currently it assumes there are only two.
+type CollectionFunctionReturnTypeWithArgs<
+	T extends CollectionsFunction,
+	Args,
+> = Extract<
+	T extends {
+		(...args: infer A1): infer R1;
+		(...args: infer A2): infer R2;
+	}
+		? [A1, R1] | [A2, R2]
+		: never,
+	[Args, unknown]
+>[1];
+type Collections = CollectionFunctionReturnTypeWithArgs<
+	CollectionsFunction,
+	[]
+>;
+type CreateSchema = Parameters<Collections["create"]>[0];
 
 export const postSchema = {
 	name: "posts",
