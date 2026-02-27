ad77e1ac55724f12276d0a90f23f398d08609e54
Furkan Emin Can <78358128+femincan@users.noreply.github.com>
2026-02-27 18:41:03 +0300

    docs: add jsdoc comments to functions of sticky header functionality

--- src/utils/sticky-table-header-script.ts
+++ src/utils/sticky-table-header-script.ts
@@ -1,3 +1,7 @@
+/**
+ * Sets the `data-sticky-headers` attribute on each `.table-container`
+ * on overflow and removes it when not overflowing.
+ */
 function updateStickyHeaderState(
 	items: (ResizeObserverEntry | HTMLDivElement)[],
 ) {
@@ -19,6 +23,11 @@ function updateStickyHeaderState(
 	}
 }
 
+/**
+ * Finds all `.table-container` elements on the page, observes them with a
+ * `ResizeObserver` to react to overlflow by adding `data-sticky-headers` attribute on overflow.
+ * Also run this on first run for intial overflow check before any resize.
+ */
 export function enableStickyTableHeaderListener() {
 	const tableContainers = Array.from(
 		document.querySelectorAll<HTMLDivElement>(".table-container"),
