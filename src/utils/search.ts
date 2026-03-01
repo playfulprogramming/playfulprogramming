f4ad65da52dfd807f80ec1f2bf817f6948a1af74
Furkan Emin Can <78358128+femincan@users.noreply.github.com>
2026-03-01 18:23:08 +0300

    refactor: use OverloadReturnType for return type resolution

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -1,23 +1,8 @@
 import { Client } from "typesense";
+import { OverloadReturnType } from "./types";
 
-type CollectionsFunction = Client["collections"];
-// This type is for getting the expected overload signature by argument types from collections function overload. It may break in a future update of the library if more overload signatures added for collections function because currently it assumes there are only two.
-type CollectionFunctionReturnTypeWithArgs<
-	T extends CollectionsFunction,
-	Args,
-> = Extract<
-	T extends {
-		(...args: infer A1): infer R1;
-		(...args: infer A2): infer R2;
-	}
-		? [A1, R1] | [A2, R2]
-		: never,
-	[Args, unknown]
->[1];
-type Collections = CollectionFunctionReturnTypeWithArgs<
-	CollectionsFunction,
-	[]
->;
+type CollectionsOverload = Client["collections"];
+type Collections = OverloadReturnType<CollectionsOverload, []>;
 type CreateSchema = Parameters<Collections["create"]>[0];
 
 export const postSchema = {
