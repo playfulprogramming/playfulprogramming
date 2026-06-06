e3c57ae8d87e57dd2c61becb7f549bfef78a531a
Corbin Crutchley <git@crutchcorn.dev>
2026-06-06 03:11:12 -0700

    chore: fix symlink issue

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import icon from "astro-icon";
-import symlink from "symlink-dir";
+import { symlinkDir } from "symlink-dir";
 import * as path from "path";
 import type { AstroUserConfig } from "astro";
 import node from "@astrojs/node";
 
-await symlink(path.resolve("content"), path.resolve("public/content"));
+await symlinkDir(path.resolve("content"), path.resolve("public/content"));
 
 const isServerBuild = process.env.BUILD_OUTPUT === "server";
 
