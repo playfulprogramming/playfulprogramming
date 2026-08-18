914e313ad2b748109a394a659bade67d09c6e617
Eduardo Pratti <eduardobumbel@gmail.com>
2026-08-18 09:56:01 -0300

    Implemented complementary rule, added dynamic theming to bowtie only when randomized, removed an unnecessary class that was hiding the randomize button.

--- src/views/base/navigation/randomize-button/randomize-button.ts
+++ src/views/base/navigation/randomize-button/randomize-button.ts
@@ -7,9 +7,12 @@ export const randomizeButtonListener = () => {
 
 	randomizeBtn.addEventListener("click", () => {
 		const root = document.documentElement;
-		const primary = String(randHue());
-		const secondary = String(randHue());
-		root.style.setProperty("--primary-hue", primary);
-		root.style.setProperty("--secondary-hue", secondary);
+		const primary = randHue();
+		const secondary = (primary + 120) % 360;
+		root.style.setProperty("--primary-hue", String(primary));
+		root.style.setProperty("--secondary-hue", String(secondary));
+
+		root.style.setProperty("--sticker_bowtie-dot", `var(--secondary70)`);
+		root.style.setProperty("--sticker_bowtie", `var(--secondary30)`);
 	});
 };
