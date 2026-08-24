3869978e7acd8e02cbca55dbf6b27c04a72ac715
Corbin Crutchley <git@crutchcorn.dev>
2026-08-24 00:48:22 -0700

    feat(i18n): translate shared page metadata

--- src/constants/site-config.ts
+++ src/constants/site-config.ts
@@ -4,11 +4,8 @@ export const siteUrl = env.SITE_URL;
 
 export const siteMetadata = {
 	title: `Playful Programming`,
-	description: `Learning programming from magically majestic words. A place to learn about all sorts of programming topics from entry-level concepts to advanced abstractions`,
 	siteUrl,
 	repoPath: "playfulprogramming/playfulprogramming",
 	relativeToPosts: "/content/blog",
-	keywords:
-		"programming,development,mobile,web,game,playful,software engineering,javascript,angular,react,computer science",
 	twitterHandle: "@playful_program",
 };
