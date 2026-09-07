e22f3bdc421ab3522cb774c93bdaf99d1e1aefd9
James Fenn <me@jfenn.me>
2026-09-07 05:36:27 -0400

    reject additional snitip fields

--- src/types/SnitipInfo.ts
+++ src/types/SnitipInfo.ts
@@ -1,18 +1,23 @@
 import Type, { type Static } from "typebox";
 import type { TagInfo } from "./TagInfo.ts";
 
-export const SnitipInfoSchema = Type.Object({
-	icon: Type.Optional(Type.String()),
-	title: Type.String(),
-	links: Type.Array(
-		Type.Object({
-			name: Type.String(),
-			href: Type.String(),
-		}),
-		{ default: [] },
-	),
-	tags: Type.Array(Type.String(), { default: [] }),
-});
+export const SnitipInfoSchema = Type.Object(
+	{
+		icon: Type.Optional(Type.String()),
+		title: Type.String(),
+		links: Type.Array(
+			Type.Object({
+				name: Type.String(),
+				href: Type.String(),
+			}),
+			{ default: [] },
+		),
+		tags: Type.Array(Type.String(), { default: [] }),
+	},
+	{
+		additionalProperties: false,
+	},
+);
 
 export type RawSnitipInfo = Static<typeof SnitipInfoSchema>;
 
