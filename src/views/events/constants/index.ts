7702022a721e16a90d532c6fcdd5128e33bbe984
Corbin Crutchley <git@crutchcorn.dev>
2026-09-10 23:58:44 -0700

    Remove office hours from the events calendar

--- src/views/events/constants/index.ts
+++ src/views/events/constants/index.ts
@@ -189,23 +189,6 @@ for (const _ of new Array(5).fill(0)) {
 	});
 }
 
-export const officeHoursBlocks = new Array(64).fill(0).map(
-	(_, index) =>
-		({
-			slug: "office-hours",
-			starts_at: parseDateTime("2026-04-02T18:00")
-				.add({ weeks: index })
-				.toDate(eventTimeZone),
-			ends_at: parseDateTime("2027-04-02T18:00")
-				.add({ weeks: index })
-				.toDate(eventTimeZone),
-			location_description: "Discord",
-			location_url: "https://discord.playfulprogramming.com",
-			presenters: [],
-			timezone: "America/Los_Angeles",
-		}) satisfies EventBlock,
-);
-
 export const SacramentoBootcampBlocks = [
 	{
 		slug: "sacramento-bootcamp-2026-01",
@@ -246,19 +229,5 @@ export const events = [
 		is_online: true,
 		is_recurring: false,
 		has_event_page: true,
-	},
-	{
-		slug: "office-hours",
-		title: "Office Hours",
-		description:
-			"This is a time for us all to hang out and get help with anything development-related - whether it’s a project, career advice, or anything else that comes to mind.",
-		location_description: "",
-		location_url: "",
-		blocks: officeHoursBlocks,
-		organizers: [],
-		in_person: false,
-		is_online: true,
-		is_recurring: true,
-		has_event_page: false,
 	},
 ] satisfies Event[];
